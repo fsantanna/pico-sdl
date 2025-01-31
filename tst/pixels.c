d4102ae28126032fe31382d3160faaa6c4337fe9
pgvalle <pgvalle.nogueira@gmail.com>
2025-01-31 19:15:42 -0300

    Fixed pixels example

--- tst/pixels.c
+++ tst/pixels.c
@@ -3,21 +3,22 @@
 int main (void) {
     pico_init(1);
     pico_set_title("pixels");
-    pico_set_anchor(PICO_LEFT, PICO_TOP);
+    pico_set_size_internal((SDL_Point){5, 5});
+    pico_set_size_external((SDL_Point){100, 100});
 
     SDL_Point pixels[9] = {
-        {0, 0}, {1, 0}, {2, 0},
-        {0, 1}, {1, 1}, {2, 1},
-        {0, 2}, {1, 2}, {2, 2}
+        {1, 1}, {2, 1}, {3, 1},
+        {1, 2}, {2, 2}, {3, 2},
+        {1, 3}, {2, 3}, {3, 3}
     };
 
     printf("press any key to start\n");
     pico_input_event(NULL, SDL_KEYDOWN);
     
-    for (int i = 0; i < 9; i++) {
+    for (int i = 1; i <= 9; i++) {
         pico_output_clear();
         pico_output_draw_pixels(pixels, i);
-        printf("%d pixels\n", i + 1);
+        printf("%d pixels\n", i);
         pico_output_present();
         pico_input_event(NULL, SDL_KEYDOWN);
     }
