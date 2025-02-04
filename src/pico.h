b39457c7ab38e6d73f2ce8e50ff66eca23c98d4a
pgvalle <pgvalle.nogueira@gmail.com>
2025-02-04 10:25:59 -0300

    removing 2 dots

--- src/pico.h
+++ src/pico.h
@@ -1,4 +1,4 @@
-// TODO: Code examples for all functions.
+// TODO Code examples for all functions.
 
 #ifndef PICO_H
 #define PICO_H
@@ -26,7 +26,7 @@ extern "C" {
 
 /// @brief Initializes and terminates pico.
 /// @include init.c
-/// @param on: 1 to initialize, or 0 to terminate
+/// @param on 1 to initialize, or 0 to terminate
 void pico_init (int on);
 /// @example init.c
 
@@ -38,22 +38,22 @@ void pico_init (int on);
 
 /// @brief Stops the program until a given number of milliseconds have passed.
 /// @include delay.c
-/// @param ms: milliseconds to wait
+/// @param ms milliseconds to wait
 void pico_input_delay (int ms);
 /// @example delay.c
 
 /// @brief Stops the program until an event occurs.
 /// @include event.c
-/// @param evt: where to save the event data, or NULL to ignore
-/// @param type: type of event to wait for (SDL_EventType), where 0 means any type
+/// @param evt where to save the event data, or NULL to ignore
+/// @param type type of event to wait for (SDL_EventType), where 0 means any type
 /// @sa pico_input_event_ask
 /// @sa pico_input_event_timeout
 void pico_input_event (SDL_Event* evt, int type);
 /// @example event.c
 
 /// @brief Checks if an event has occured.
-/// @param evt: where to save the event data, or NULL to ignore
-/// @param type: type of event to check the occurence (SDL_EventType), where 0 means any type
+/// @param evt where to save the event data, or NULL to ignore
+/// @param type type of event to check the occurence (SDL_EventType), where 0 means any type
 /// @return 1 if the given type of event has occurred, or 0 otherwise
 /// @sa pico_input_event
 /// @sa pico_input_event_timeout
@@ -61,9 +61,9 @@ int  pico_input_event_ask (SDL_Event* evt, int type);
 
 /// @brief Stops the program until an event occurs or a timeout is reached.
 /// @include event_timeout.c
-/// @param evt: where to save the event data, or NULL to ignore
-/// @param type: type of event to wait for (SDL_EventType), where 0 means any type
-/// @param timeout: time limit to wait for events in milliseconds
+/// @param evt where to save the event data, or NULL to ignore
+/// @param type type of event to wait for (SDL_EventType), where 0 means any type
+/// @param timeout time limit to wait for events in milliseconds
 /// @return 1 if the given type of event has occurred, or 0 otherwise
 /// @sa pico_input_event
 /// @sa pico_input_event_ask
@@ -81,35 +81,35 @@ void pico_output_clear (void);
 
 /// @brief Draws an image.
 /// This function uses caching, so the file is actually loaded only once.
-/// @param pos: drawing position
-/// @param path: path to the image file
+/// @param pos drawing position
+/// @param path path to the image file
 void pico_output_draw_image (SDL_Point pos, const char* path);
 
 /// @brief Draws a line segment.
-/// @param p1: first point
-/// @param p2: second point
+/// @param p1 first point
+/// @param p2 second point
 void pico_output_draw_line (SDL_Point p1, SDL_Point p2);
 
 /// @brief Draws a single pixel.
-/// @param pos: drawing position
+/// @param pos drawing position
 void pico_output_draw_pixel (SDL_Point pos);
 
 /// @brief Draws a batch of pixels.
-/// @param apos: array of positions
-/// @param count: amount of pixels to draw
+/// @param apos array of positions
+/// @param count amount of pixels to draw
 void pico_output_draw_pixels (const SDL_Point* apos, int count);
 
 /// @brief Draws a rectangle.
-/// @param rect: rectangle to draw
+/// @param rect rectangle to draw
 void pico_output_draw_rect (SDL_Rect rect);
 
 /// @brief Draws an ellipse.
-/// @param rect: bounds of the ellipse
+/// @param rect bounds of the ellipse
 void pico_output_draw_oval (SDL_Rect rect);
 
 /// @brief Draws text. The string can't be empty.
-/// @param pos: drawing position
-/// @param text: text to draw
+/// @param pos drawing position
+/// @param text text to draw
 void pico_output_draw_text (SDL_Point pos, const char* text);
 
 /// @brief Shows what has been drawn onto the screen.
@@ -117,13 +117,13 @@ void pico_output_present (void);
 
 /// @brief Plays a sound.
 /// This function uses caching, so the file is actually loaded only once.
-/// @param path: path to the audio file
+/// @param path path to the audio file
 void pico_output_sound (const char* path);
 
-// TODO: Document me
+// TODO Document me
 void pico_output_write (const char* text);
 
-// TODO: Document me
+// TODO Document me
 void pico_output_writeln (const char* text);
 
 void _pico_output_draw_image_cache (SDL_Point pos, const char* path, int cache);
@@ -148,7 +148,7 @@ typedef enum {
 } Pico_VAnchor;
 
 /// @brief Returns the size of a given image.
-/// @param file: path to image file
+/// @param file path to image file
 SDL_Point pico_get_image_size (const char* file);
 
 /// @brief Returns the physical window size.
@@ -170,79 +170,79 @@ SDL_Point pico_get_size_internal (void);
 /// @brief Returns the amount of ticks that passed since pico was initialized.
 Uint32 pico_get_ticks (void);
 
-// TODO: Document me better
+// TODO Document me better
 /// @brief Changes the coordinate system (anchor) of objects to draw.
-/// @param h: x-axis anchor
-/// @param v: y-axis anchor
+/// @param h x-axis anchor
+/// @param v y-axis anchor
 ///
 /// @include anchor.c
 void pico_set_anchor (Pico_HAnchor h, Pico_VAnchor v);
 
 /// @brief Changes the alpha blending mode.
-/// @param mode: new blend mode
+/// @param mode new blend mode
 void pico_set_blend (SDL_BlendMode mode);
 
 /// @brief Changes the color used to clear the screen.
-/// @param color: new color
+/// @param color new color
 /// @sa pico_output_clear
 void pico_set_color_clear (SDL_Color color);
 
 /// @brief Changes the color used to draw objects.
-/// @param color: new color
+/// @param color new color
 void pico_set_color_draw (SDL_Color color);
 
-// TODO: Document me
+// TODO Document me
 void pico_set_cursor (SDL_Point pos);
 
 /// @brief Changes the font used to draw texts.
-/// @param file: path to font file
-/// @param h: size of the font
+/// @param file path to font file
+/// @param h size of the font
 void pico_set_font (const char* file, int h);
 
 /// @brief Toggles a grid on top of logical pixels.
-/// @param on: 1 to show it, or 0 to hide it
+/// @param on 1 to show it, or 0 to hide it
 void pico_set_grid (int on);
 
 /// @brief Changes the cropping that is applied to images before drawing them.
-/// @param crop: cropping region, which may have 0 area to disable cropping
+/// @param crop cropping region, which may have 0 area to disable cropping
 void pico_set_image_crop (SDL_Rect crop);
 
 /// @brief Changes what size images should be when drawn.
-/// @param size: new size, which may be (0, 0) to disable resizing
+/// @param size new size, which may be (0, 0) to disable resizing
 void pico_set_image_size (SDL_Point size);
 
 /// @brief Changes the point of view on the logical window.
-/// @param pos: new point of view
+/// @param pos new point of view
 void pico_set_pan (SDL_Point pos);
 
 /// @brief Changes the physical window size and the logical window size.
-/// @param size: new size
+/// @param size new size
 /// @sa pico_set_size_external
 /// @sa pico_set_size_internal
 void pico_set_size (SDL_Point size);
 
 /// @brief Changes the physical window size.
-/// @param phy: new physical size
+/// @param phy new physical size
 /// @sa pico_set_size_internal
 /// @sa pico_set_size
 void pico_set_size_external (SDL_Point phy);
 
 /// @brief Changes the logical window size.
-/// @param log: new logical size
+/// @param log new logical size
 /// @sa pico_set_size_external
 /// @sa pico_set_size
 void pico_set_size_internal (SDL_Point log);
 
 /// @brief Toggles the aplication window visibility.
-/// @param on: 1 to show, or 0 to hide
+/// @param on 1 to show, or 0 to hide
 void pico_set_show (int on);
 
 /// @brief Changes the style used to draw objects.
-/// @param style: new style to use
+/// @param style new style to use
 void pico_set_style (Pico_Style style);
 
 /// @brief Changes the aplication title
-/// @param title: new title to set
+/// @param title new title to set
 void pico_set_title (const char* title);
 
 /// @}
@@ -252,19 +252,19 @@ void pico_set_title (const char* title);
 /// @{
 
 /// @brief Asserts condition and shows SDL error on failure
-/// @param x: condition to assert
+/// @param x condition to assert
 #define pico_assert(x) if (!(x)) { fprintf(stderr,"%s\n",SDL_GetError()); assert(0 && "SDL ERROR"); }
 
 /// @brief Checks if a point is inside a rectangle.
-/// @param pt: point
-/// @param r: rectangle
+/// @param pt point
+/// @param r rectangle
 /// @return 1 if pt is inside r, or 0 otherwise
 int pico_is_point_in_rect (SDL_Point pt, SDL_Rect r);
 
-// TODO: Document me
+// TODO Document me
 SDL_Point pico_pct_to_pos (float x, float y);
 
-// TODO: Document me
+// TODO Document me
 SDL_Point pico_pct_to_pos_x (SDL_Rect r, float x, float y);
 
 /// @}
