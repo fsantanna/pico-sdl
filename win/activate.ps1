a77a91a7d8f8ab15d76c555770351e086d4117d1
pgvalle <pgvalle.nogueira@gmail.com>
2025-03-14 11:12:31 -0300

    activate IDE scripts

---
+++ win/activate.ps1
@@ -0,0 +1,8 @@
+$WORKING_DIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$env:PATH = "$WORKING_DIR\mingw64\bin;$WORKING_DIR\vscode\bin;$WORKING_DIR\bin;$env:PATH"
+$env:CPATH = "$WORKING_DIR\include;$env:CPATH"
+$env:LIBRARY_PATH = "$WORKING_DIR;$env:LIBRARY_PATH"
+$env:LD_LIBRARY_PATH = "$WORKING_DIR;$env:LD_LIBRARY_PATH"
+
+Write-Host "Pico IDE activated!"
