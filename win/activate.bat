3f7d5151673ce4040f801ad6598a115adc97dcbb
pgvalle <pgvalle.nogueira@gmail.com>
2025-03-14 18:14:59 -0300

    fixed wrong path in activation script

--- win/activate.bat
+++ win/activate.bat
@@ -3,7 +3,7 @@ rem Get the directory of the batch script
 set "WORKING_DIR=%~dp0"
 
 rem Modify environment variables
-set "PATH=%WORKING_DIR%mingw\bin;%WORKING_DIR%vscode\bin;%WORKING_DIR%bin;%PATH%"
+set "PATH=%WORKING_DIR%mingw64\bin;%WORKING_DIR%vscode\bin;%WORKING_DIR%bin;%PATH%"
 set "CPATH=%WORKING_DIR%include;%CPATH%"
 set "LIBRARY_PATH=%WORKING_DIR%bin;%LIBRARY_PATH%"
 set "LD_LIBRARY_PATH=%WORKING_DIR%bin;%LD_LIBRARY_PATH%"
