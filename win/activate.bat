56c5094e7ea97548dc7a5077e23a465a2797f5f4
pgvalle <pgvalle.nogueira@gmail.com>
2025-03-14 17:12:10 -0300

    fixed activate.bat not working

--- win/activate.bat
+++ win/activate.bat
@@ -3,9 +3,9 @@ rem Get the directory of the batch script
 set "WORKING_DIR=%~dp0"
 
 rem Modify environment variables
-set "PATH=%WORKING_DIR%\gcc\bin;%WORKING_DIR%\vscode\bin;%WORKING_DIR%\bin;%PATH%"
-set "CPATH=%WORKING_DIR%\include;%CPATH%"
-set "LIBRARY_PATH=%WORKING_DIR%\bin;%LIBRARY_PATH%"
-set "LD_LIBRARY_PATH=%WORKING_DIR%\bin;%LD_LIBRARY_PATH%"
+set "PATH=%WORKING_DIR%mingw\bin;%WORKING_DIR%vscode\bin;%WORKING_DIR%bin;%PATH%"
+set "CPATH=%WORKING_DIR%include;%CPATH%"
+set "LIBRARY_PATH=%WORKING_DIR%bin;%LIBRARY_PATH%"
+set "LD_LIBRARY_PATH=%WORKING_DIR%bin;%LD_LIBRARY_PATH%"
 
-echo "Pico IDE activated!"
+echo Pico IDE activated!
