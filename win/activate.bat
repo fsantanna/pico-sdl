aa3c3ad8c9dde87ee549ffc3010e574346d174c3
pgvalle <pgvalle.nogueira@gmail.com>
2025-03-14 16:13:10 -0300

    fixed wrong environment in activate scripts

--- win/activate.bat
+++ win/activate.bat
@@ -5,7 +5,7 @@ set "WORKING_DIR=%~dp0"
 rem Modify environment variables
 set "PATH=%WORKING_DIR%\gcc\bin;%WORKING_DIR%\vscode\bin;%WORKING_DIR%\bin;%PATH%"
 set "CPATH=%WORKING_DIR%\include;%CPATH%"
-set "LIBRARY_PATH=%WORKING_DIR%;%LIBRARY_PATH%"
-set "LD_LIBRARY_PATH=%WORKING_DIR%;%LD_LIBRARY_PATH%"
+set "LIBRARY_PATH=%WORKING_DIR%\bin;%LIBRARY_PATH%"
+set "LD_LIBRARY_PATH=%WORKING_DIR%\bin;%LD_LIBRARY_PATH%"
 
 echo "Pico IDE activated!"
--- win/activate.ps1
+++ win/activate.ps1
@@ -2,7 +2,7 @@ $WORKING_DIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $env:PATH = "$WORKING_DIR\mingw64\bin;$WORKING_DIR\vscode\bin;$WORKING_DIR\bin;$env:PATH"
 $env:CPATH = "$WORKING_DIR\include;$env:CPATH"
-$env:LIBRARY_PATH = "$WORKING_DIR;$env:LIBRARY_PATH"
-$env:LD_LIBRARY_PATH = "$WORKING_DIR;$env:LD_LIBRARY_PATH"
+$env:LIBRARY_PATH = "$WORKING_DIR\bin;$env:LIBRARY_PATH"
+$env:LD_LIBRARY_PATH = "$WORKING_DIR\bin;$env:LD_LIBRARY_PATH"
 
 Write-Host "Pico IDE activated!"
