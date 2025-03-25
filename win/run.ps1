ebc2183fe335661dbb317f7467c57bae22f0bdd9
pgvalle <pgvalle.nogueira@gmail.com>
2025-03-25 01:34:54 -0300

    unsing batch to launch IDE

--- win/run.ps1
+++
@@ -1,7 +0,0 @@
-$RUN_DIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
-
-$env:PATH = "$RUN_DIR\SDL\bin;$RUN_DIR\mingw64\bin;$RUN_DIR\vscode\bin;$RUN_DIR\bin;$env:PATH"
-$env:CPATH = "$RUN_DIR\src;$RUN_DIR\SDL\include;$RUN_DIR\SDL\include\SDL2;$env:CPATH"
-$env:LIBRARY_PATH = "$RUN_DIR\SDL\lib;$env:LIBRARY_PATH"
-
-code .
