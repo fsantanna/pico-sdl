0f8456a016aa6dfa8393b0b73109ec19c358a89b
pgvalle <pgvalle.nogueira@gmail.com>
2025-03-24 23:26:21 -0300

    activate.ps1 -> run.ps1

--- win/activate.ps1
+++
@@ -1,9 +0,0 @@
-$WORKING_DIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
-
-$env:PATH = "$WORKING_DIR\mingw64\bin;$WORKING_DIR\vscode\bin;$WORKING_DIR\bin;$env:PATH"
-$env:CPATH = "$WORKING_DIR\include;$env:CPATH"
-$env:LIBRARY_PATH = "$WORKING_DIR\bin;$env:LIBRARY_PATH"
-$env:LD_LIBRARY_PATH = "$WORKING_DIR\bin;$env:LD_LIBRARY_PATH"
-
-code .
-Write-Host "Pico IDE activated!"
