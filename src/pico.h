381563ed97b47aa7f4aad45ad3206f5587f316d1
pgvalle <pgvalle.nogueira@gmail.com>
2025-07-15 12:45:57 -0300

    removed off_to_pos

--- src/pico.h
+++ src/pico.h
@@ -290,8 +290,6 @@ Pico_Pos pico_pct_to_pos (int x, int y);
 /// @sa pico_pct_to_pos
 Pico_Pos pico_pct_to_pos_ext (Pico_Rect r, int x, int y);
 
-Pico_Pos pico_off_to_pos (Pico_Rect r, int x, int y);
-
 /// @}
 
 #ifdef __cplusplus
