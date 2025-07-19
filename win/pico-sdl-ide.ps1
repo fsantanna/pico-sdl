c8b61eb495f75820a73fc9730dbc2090dd36e8ee
Pedro Gabriel <pgvalle.nogueira@gmail.com>
2025-07-19 16:57:26 -0300

    Forgot vscode path

--- win/pico-sdl-ide.ps1
+++ win/pico-sdl-ide.ps1
@@ -1,6 +1,6 @@
 $PICO = $PSScriptRoot
 
-$env:PATH = "$PICO;$PICO\SDL\bin;$PICO\mingw64\bin;" + $env:PATH
+$env:PATH = "$PICO;$PICO\SDL\bin;$PICO\mingw64\bin;$PICO\vscode\bin;" + $env:PATH
 $env:CPATH = "$PICO\src;$PICO\SDL\include;$PICO\SDL\include\SDL2;" + $env:CPATH
 $env:LIBRARY_PATH = "$PICO\SDL\lib;" + $env:LIBRARY_PATH
 
