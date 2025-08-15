eb9165ed08edec397c10e3da55ff69e1ef2bf87e
pgvalle <pgvalle.nogueira@gmail.com>
2025-08-15 11:52:06 -0300

    test: anchors with pct out of [0,100]

--- tst/anchor.c
+++ tst/anchor.c
@@ -74,6 +74,37 @@ int main (void) {
         pico_input_event(NULL, PICO_KEYDOWN);
     }
 
+    // RECTS out of [0,100]
+    {
+        pico_set_anchor((Pico_Anchor){25, 25});
+        Pico_Pos pt = pico_pos(20, 20);
+        Pico_Rect rct = { pt.x, pt.y, 4, 4 };
+        pico_output_clear();
+        pico_output_draw_rect(rct);
+        puts("ancora 25/25 - top-left 10/10");
+        pico_input_event(NULL, PICO_KEYDOWN);
+    }
+
+    {
+        pico_set_anchor((Pico_Anchor){-25, -25});
+        Pico_Pos pt = pico_pos(50, 50);
+        Pico_Rect rct = { pt.x, pt.y, 4, 4 };
+        pico_output_clear();
+        pico_output_draw_rect(rct);
+        puts("ancora -25/-25 - top-left 60/60");
+        pico_input_event(NULL, PICO_KEYDOWN);
+    }
+
+    {
+        pico_set_anchor((Pico_Anchor){125, 125});
+        Pico_Pos pt = pico_pos(50, 50);
+        Pico_Rect rct = { pt.x, pt.y, 4, 4 };
+        pico_output_clear();
+        pico_output_draw_rect(rct);
+        puts("retangulo 125/125 - bottom-right 40/40");
+        pico_input_event(NULL, PICO_KEYDOWN);
+    }
+
     pico_init(0);
     return 0;
 }
