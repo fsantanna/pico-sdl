e62cfecf1ed013556672e6df64daf0867de37868
pgvalle <pgvalle.nogueira@gmail.com>
2025-08-15 20:35:01 -0300

    refactor: buffer test

--- tst/buffer.c
+++ tst/buffer.c
@@ -4,6 +4,7 @@ int main (void) {
     pico_init(1);
     pico_set_title("Buffer");
     pico_set_size((Pico_Dim){100,100}, (Pico_Dim){10,10});
+    pico_output_clear();
 
     // .x.
     // xxx
@@ -24,17 +25,17 @@ int main (void) {
     Pico_Pos p1 = pico_pos(50,50);
     pico_output_draw_buffer(p1, buffer, (Pico_Dim){3,3});
 
-    printf("press any key\n");
+    puts("press any key");
     pico_input_event(NULL, PICO_KEYDOWN);
 
     puts("bottom right multi-color '.x.xxx.x.' over white");
     pico_set_color_clear((Pico_Color){0xFF, 0xFF, 0xFF, 0xFF});
     pico_output_clear();
     Pico_Pos p2 = pico_pos(100,100);
-    pico_set_anchor(PICO_RIGHT, PICO_BOTTOM);
+    pico_set_anchor((Pico_Anchor){PICO_RIGHT, PICO_BOTTOM});
     pico_output_draw_buffer(p2, buffer, (Pico_Dim){9,1});
     
-    printf("press any key\n");
+    puts("press any key");
     pico_input_event(NULL, PICO_KEYDOWN);
 
     pico_init(0);
