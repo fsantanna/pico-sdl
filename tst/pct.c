df0c3fcb5b62198294872d95ddbb7bf892f3b346
pgvalle <pgvalle.nogueira@gmail.com>
2025-08-18 17:42:22 -0300

    test: testing pcts out of [0,100] for pico_pos_ext

--- tst/pct.c
+++ tst/pct.c
@@ -64,7 +64,7 @@ int main (void) {
     }
 
     {
-        puts("rect at 110/110 (bottom-right)");
+        puts("rect at 110/110 (bottom-right; symmetric to previous)");
         pico_output_clear();
 
         Pico_Pos pt = pico_pos(110, 110);
@@ -72,6 +72,46 @@ int main (void) {
         pico_set_anchor((Pico_Anchor){PICO_RIGHT, PICO_BOTTOM});
         pico_set_color_draw((Pico_Color){0xFF,0xFF,0xFF,0xFF});
         pico_output_draw_rect(rct);
+
+        pico_input_event(NULL, PICO_KEYDOWN);
+    }
+
+    {
+        puts("rect at 50% anchored by bottom-right");
+        pico_output_clear();
+
+        Pico_Pos pt1 = pico_pos(50, 50);
+        Pico_Rect rct1 = {pt1.x, pt1.y, 32, 18};
+        pico_set_anchor((Pico_Anchor){PICO_CENTER, PICO_MIDDLE});
+        pico_set_color_draw((Pico_Color){0xFF,0xFF,0xFF,0xFF});
+        pico_output_draw_rect(rct1);
+
+        puts("red anchored by top-left under -10% of white");
+        Pico_Pos pt2 = pico_pos_ext(rct1, -10, -10);
+        Pico_Rect rct2 = (Pico_Rect) {pt2.x, pt2.y, 16, 9};
+        pico_set_anchor((Pico_Anchor){PICO_LEFT, PICO_TOP});
+        pico_set_color_draw((Pico_Color){255,0,0,255});
+        pico_output_draw_rect(rct2);
+
+        pico_input_event(NULL, PICO_KEYDOWN);
+    }
+
+    {
+        puts("rect at 50%");
+        pico_output_clear();
+
+        Pico_Pos pt1 = pico_pos(50, 50);
+        Pico_Rect rct1 = {pt1.x, pt1.y, 32, 18};
+        pico_set_anchor((Pico_Anchor){PICO_CENTER, PICO_MIDDLE});
+        pico_set_color_draw((Pico_Color){0xFF,0xFF,0xFF,0xFF});
+        pico_output_draw_rect(rct1);
+
+        puts("red anchored by top-left under 110% of white (symmetric to previous)");
+        Pico_Pos pt2 = pico_pos_ext(rct1, 110, 110);
+        Pico_Rect rct2 = (Pico_Rect) {pt2.x, pt2.y, 16, 9};
+        pico_set_anchor((Pico_Anchor){PICO_RIGHT, PICO_BOTTOM});
+        pico_set_color_draw((Pico_Color){255,0,0,255});
+        pico_output_draw_rect(rct2);
 
         pico_input_event(NULL, PICO_KEYDOWN);
     }
