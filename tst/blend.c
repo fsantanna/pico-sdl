ce853dbb8405653e8ee8d3f07844f827f3f35c4e
pgvalle <pgvalle.nogueira@gmail.com>
2025-08-22 19:11:18 -0300

    proper blend testing

--- tst/blend.c
+++ tst/blend.c
@@ -1,54 +1,86 @@
 #include "pico.h"
+#define PICO_DEBUG_TESTS
+#include "tst.c"
 
 int main (void) {
     pico_init(1);
     pico_set_title("Blend");
-    pico_set_grid(0);
+
     Pico_Pos pos = pico_pos(50, 50);
     Pico_Rect rct = {pos.x, pos.y, 60, 12};
 
-    // PIXEL
-    for (int a = 255; a > 0; a-=5) {
-        pico_output_clear();
-        pico_set_color_draw((Pico_Color){255,0,0,a});
-        pico_output_draw_pixel(pos);
-        pico_input_delay(50);
-    }
-
-    // TEXT
-    for (int a = 255; a > 0; a-=5) {
-        pico_output_clear();
-        pico_set_color_draw((Pico_Color){255,0,0,a});
-        pico_output_draw_text(pos, "SOME TEXT");
-        pico_input_delay(50);
-    }
-
-    // RECT
-    for (int a = 255; a > 0; a-=5) {
-        pico_output_clear();
-        pico_set_color_draw((Pico_Color){255,0,0,a});
-        pico_output_draw_rect(rct);
-        pico_input_delay(50);
-    }
-
-    // OVAL
-    for (int a = 255; a > 0; a-=5) {
-        pico_output_clear();
-        pico_set_color_draw((Pico_Color){255,0,0,a});
-        pico_output_draw_oval(rct);
-        pico_input_delay(50);
-    }
-
-    // TRI
-    for (int a = 255; a > 0; a-=5) {
-        pico_output_clear();
-        pico_set_color_draw((Pico_Color){255,0,0,a});
-        pico_output_draw_tri(rct);
-        pico_input_delay(50);
-    }
-
-    // LINE
     {
+        puts("pixel dimming");
+        for (int a = 255; a > 0; a-=5) {
+            pico_output_clear();
+            pico_set_color_draw((Pico_Color){255,0,0,a});
+            pico_output_draw_pixel(pos);
+            pico_input_delay(50);
+            if (a == 120) {
+                puts("screenshot pixel");
+                _pico_check_assert("pixel_dimmed");
+            }
+        }
+    }
+    
+    {
+        puts("text dimming");
+        for (int a = 255; a > 0; a-=5) {
+            pico_output_clear();
+            pico_set_color_draw((Pico_Color){255,0,0,a});
+            pico_output_draw_text(pos, "SOME TEXT");
+            pico_input_delay(50);
+            if (a == 120) {
+                puts("screenshot text");
+                _pico_check_assert("text_dimmed");
+            }
+        }
+    }
+
+    {
+        puts("rect dimming");
+        for (int a = 255; a > 0; a-=5) {
+            pico_output_clear();
+            pico_set_color_draw((Pico_Color){255,0,0,a});
+            pico_output_draw_rect(rct);
+            pico_input_delay(50);
+            if (a == 120) {
+                puts("screenshot rect");
+                _pico_check_assert("rect_dimmed");
+            }
+        }
+    }
+    
+    {
+        puts("oval dimming");
+        for (int a = 255; a > 0; a-=5) {
+            pico_output_clear();
+            pico_set_color_draw((Pico_Color){255,0,0,a});
+            pico_output_draw_oval(rct);
+            pico_input_delay(50);
+            if (a == 120) {
+                puts("screenshot oval");
+                _pico_check_assert("oval_dimmed");
+            }
+        }
+    }
+
+    {
+        puts("tri dimming");
+        for (int a = 255; a > 0; a-=5) {
+            pico_output_clear();
+            pico_set_color_draw((Pico_Color){255,0,0,a});
+            pico_output_draw_tri(rct);
+            pico_input_delay(50);
+            if (a == 120) {
+                puts("screenshot tri");
+                _pico_check_assert("tri_dimmed");
+            }
+        }
+    }
+
+    {
+        puts("line dimming");
         Pico_Pos p1 = {pos.x-20, pos.y-6};
         Pico_Pos p2 = {pos.x+20, pos.y+6};
         for (int a = 255; a > 0; a-=5) {
@@ -56,21 +88,27 @@ int main (void) {
             pico_set_color_draw((Pico_Color){255,0,0,a});
             pico_output_draw_line(p1, p2);
             pico_input_delay(50);
+            if (a == 120) {
+                puts("screenshot line");
+                _pico_check_assert("line_dimmed");
+            }
         }
     }
 
-    // POLY
     {
+        puts("poly dimming");
         Pico_Pos poly[] = {{5, 5}, {59, 10}, {20, 31}};
         for (int a = 255; a > 0; a-=5) {
             pico_output_clear();
             pico_set_color_draw((Pico_Color){255,0,0,a});
             pico_output_draw_poly(poly, 3);
             pico_input_delay(50);
+            if (a == 120) {
+                puts("screenshot poly");
+                _pico_check_assert("poly_dimmed");
+            }
         }
     }
-    
-    pico_input_event(NULL, PICO_KEYDOWN);
 
     pico_init(0);
     return 0;
