bc31955d0d69c86c66a678d27206bab593908a68
pgvalle <pgvalle.nogueira@gmail.com>
2025-08-21 22:39:51 -0300

    testing blend

--- tst/blend.c
+++ tst/blend.c
@@ -4,27 +4,68 @@ int main (void) {
     pico_init(1);
     pico_set_title("Blend");
     pico_set_grid(0);
-    pico_set_anchor(PICO_LEFT, PICO_TOP);
+    Pico_Pos pos = pico_pos(50, 50);
+
+    // TEXT
     
     pico_output_clear();
     pico_set_color_draw((Pico_Color){255,255,255,255});
-    pico_output_draw_text((Pico_Pos){0, 0}, "opaque");
-
-    pico_input_event(NULL, PICO_KEYDOWN);
+    pico_output_draw_text(pos, "SOME TEXT");
+    pico_input_delay(2000);
 
     pico_output_clear();
-    pico_set_color_draw((Pico_Color){255,255,255,122});
-    pico_output_draw_text((Pico_Pos){0, 0}, "dimmed");
+    pico_set_color_draw((Pico_Color){255,255,255,120});
+    pico_output_draw_text(pos, "SOME TEXT");
+    pico_input_delay(2000);
 
-    pico_input_event(NULL, PICO_KEYDOWN);
+    // RECT
 
     pico_output_clear();
     pico_set_color_draw((Pico_Color){255,255,255,255});
-    pico_output_draw_text((Pico_Pos){0, 0}, "dimmed by rect");
-    pico_set_color_draw((Pico_Color){0,0,0,122});
-    pico_output_draw_rect((Pico_Rect){0, 0, 112, 8});
+    pico_output_draw_rect((Pico_Rect){pos.x, pos.y, 60, 12});
+    pico_input_delay(2000);
 
-    pico_input_event(NULL, PICO_KEYDOWN);
+    pico_output_clear();
+    pico_set_color_draw((Pico_Color){255,255,255,120});
+    pico_output_draw_rect((Pico_Rect){pos.x, pos.y, 60, 12});
+    pico_input_delay(2000);
+
+    // OVAL
+    pico_set_color_clear((Pico_Color){255,120,120,255});
+
+    pico_output_clear();
+    pico_set_color_draw((Pico_Color){255,255,255,255});
+    pico_output_draw_oval((Pico_Rect){pos.x, pos.y, 60, 12});
+    pico_input_delay(2000);
+
+    pico_output_clear();
+    pico_set_color_draw((Pico_Color){255,255,255,120});
+    pico_output_draw_oval((Pico_Rect){pos.x, pos.y, 60, 12});
+    pico_input_delay(2000);
+
+    // TRI
+
+    pico_output_clear();
+    pico_set_color_draw((Pico_Color){255,255,255,255});
+    pico_output_draw_tri((Pico_Rect){pos.x, pos.y, 60, 12});
+    pico_input_delay(2000);
+
+    pico_output_clear();
+    pico_set_color_draw((Pico_Color){255,255,255,120});
+    pico_output_draw_tri((Pico_Rect){pos.x, pos.y, 60, 12});
+    pico_input_delay(2000);
+
+    // LINE
+
+    pico_output_clear();
+    pico_set_color_draw((Pico_Color){255,255,255,255});
+    pico_output_draw_line((Pico_Pos){pos.x-20, pos.y-6}, (Pico_Pos){pos.x+20,pos.y+6});
+    pico_input_delay(2000);
+
+    pico_output_clear();
+    pico_set_color_draw((Pico_Color){255,255,255,120});
+    pico_output_draw_line((Pico_Pos){pos.x-20, pos.y-6}, (Pico_Pos){pos.x+20,pos.y+6});
+    pico_input_delay(2000);
 
     pico_init(0);
     return 0;
