9f57eab6eb5d626c255f4971ed85aab1d8ef7776
pgvalle <pgvalle.nogueira@gmail.com>
2025-08-22 18:49:51 -0300

    anchor.c: refactor for new apis + dbg msgs

--- tst/anchor.c
+++ tst/anchor.c
@@ -8,92 +8,102 @@ int main (void) {
 
     // PIXELS
     {
-        pico_set_anchor((Pico_Anchor){PICO_CENTER, PICO_MIDDLE});
+        puts("centered pixel - 1dir/1baixo");
+        pico_set_anchor_draw((Pico_Anchor){PICO_CENTER, PICO_MIDDLE});
         Pico_Pos pt = pico_pos(50, 50);
         pico_output_clear();
         pico_output_draw_pixel(pt);
-        _pico_assert_output("pixel50x50y_center");
+        _pico_check_assert("pixel50x50y_center");
     }
 
     {
-        pico_set_anchor((Pico_Anchor){PICO_LEFT, PICO_TOP});
+        puts("centered pixel - 1dir/1baixo");
+        pico_set_anchor_draw((Pico_Anchor){PICO_LEFT, PICO_TOP});
         Pico_Pos pt = pico_pos(50, 50);
         pico_output_clear();
         pico_output_draw_pixel(pt);
-        _pico_assert_output("pixel50x50y_lefttop");
+        _pico_check_assert("pixel50x50y_lefttop");
     }
 
     {
-        pico_set_anchor((Pico_Anchor){PICO_RIGHT, PICO_BOTTOM});
+        puts("centered pixel - 1esq/1cima");
+        pico_set_anchor_draw((Pico_Anchor){PICO_RIGHT, PICO_BOTTOM});
         Pico_Pos pt = pico_pos(50, 50);
         pico_output_clear();
         pico_output_draw_pixel(pt);
-        _pico_assert_output("pixel50x50y_rightbottom");
+        _pico_check_assert("pixel50x50y_rightbottom");
     }
 
     // RECTS
     {
-        pico_set_anchor((Pico_Anchor){PICO_CENTER, PICO_MIDDLE});
+        puts("centered rect - exact");
+        pico_set_anchor_draw((Pico_Anchor){PICO_CENTER, PICO_MIDDLE});
         Pico_Pos pt = pico_pos(50, 50);
         Pico_Rect rct = { pt.x, pt.y, 4, 4 };
         pico_output_clear();
         pico_output_draw_rect(rct);
-        _pico_assert_output("rect50x50y_center");
+        _pico_check_assert("rect50x50y_center");
     }
 
     {
-        pico_set_anchor((Pico_Anchor){PICO_LEFT, PICO_TOP});
+        puts("topleft centered - 1lin/1col");
+        pico_set_anchor_draw((Pico_Anchor){PICO_LEFT, PICO_TOP});
         Pico_Pos pt = pico_pos(50, 50);
         Pico_Rect rct = { pt.x, pt.y, 4, 4 };
         pico_output_clear();
         pico_output_draw_rect(rct);
-        _pico_assert_output("rect50x50y_lefttop");
+        _pico_check_assert("rect50x50y_lefttop");
     }
 
     {
-        pico_set_anchor((Pico_Anchor){PICO_RIGHT, PICO_BOTTOM});
+        puts("bottomright centered - 1lin/1col");
+        pico_set_anchor_draw((Pico_Anchor){PICO_RIGHT, PICO_BOTTOM});
         Pico_Pos pt = pico_pos(50, 50);
         Pico_Rect rct = { pt.x, pt.y, 4, 4 };
         pico_output_clear();
         pico_output_draw_rect(rct);
-        _pico_assert_output("rect50x50y_rightbottom");
+        _pico_check_assert("rect50x50y_rightbottom");
     }
 
     {
-        pico_set_anchor((Pico_Anchor){PICO_RIGHT, PICO_MIDDLE});
+        puts("rightmiddle centered - 1col");
+        pico_set_anchor_draw((Pico_Anchor){PICO_RIGHT, PICO_MIDDLE});
         Pico_Pos pt = pico_pos(50, 50);
         Pico_Rect rct = { pt.x, pt.y, 4, 4 };
         pico_output_clear();
         pico_output_draw_rect(rct);
-        _pico_assert_output("rect50x50y_rightcenter");
+        _pico_check_assert("rect50x50y_rightcenter");
     }
 
     {
-        pico_set_anchor((Pico_Anchor){25, 25});
+        puts("anchor 25%25% 20%20% - 1lin/1col");
+        pico_set_anchor_draw((Pico_Anchor){25, 25});
         Pico_Pos pt = pico_pos(20, 20);
         Pico_Rect rct = { pt.x, pt.y, 4, 4 };
         pico_output_clear();
         pico_output_draw_rect(rct);
-        _pico_assert_output("rect20x20y_25x25y");
+        _pico_check_assert("rect20x20y_25x25y");
     }
     
     // RECTS out of [0,100]
     {
-        pico_set_anchor((Pico_Anchor){-25, -25});
+        puts("anchor -25%-25% centered - touching border");
+        pico_set_anchor_draw((Pico_Anchor){-25, -25});
         Pico_Pos pt = pico_pos(50, 50);
         Pico_Rect rct = { pt.x, pt.y, 4, 4 };
         pico_output_clear();
         pico_output_draw_rect(rct);
-        _pico_assert_output("rect20x20y_-25x-25y");
+        _pico_check_assert("rect20x20y_-25x-25y");
     }
 
     {
-        pico_set_anchor((Pico_Anchor){125, 125});
+        puts("anchor 125%125% centered - touching border");
+        pico_set_anchor_draw((Pico_Anchor){125, 125});
         Pico_Pos pt = pico_pos(50, 50);
         Pico_Rect rct = { pt.x, pt.y, 4, 4 };
         pico_output_clear();
         pico_output_draw_rect(rct);
-        _pico_assert_output("rect50x50y_125x125y");
+        _pico_check_assert("rect50x50y_125x125y");
     }
 
     pico_init(0);
