3f9a76f0736a54b0d447cec4e70a0b86e8f690f8
Francisco Sant'Anna <francisco.santanna@gmail.com>
2025-11-12 13:45:24 -0300

    - Lua: pico.set.title, pico.input.delay, pico.input.event

--- src/lua/pico.c
+++ src/lua/pico.c
@@ -1,7 +1,13 @@
+// gcc -shared -o pico.so -fPIC /x/pico-sdl/src/pico.c ../hash.c ../dir.c pico.c -llua5.4 -lSDL2 -lSDL2_gfx -lSDL2_ttf -lSDL2_mixer -lSDL2_image
+
 #include <lua5.4/lua.h>
 #include <lua5.4/lauxlib.h>
 
 #include "../pico.h"
+
+static const char KEY;
+
+///////////////////////////////////////////////////////////////////////////////
 
 static int l_init (lua_State* L) {
     luaL_checktype(L, 1, LUA_TBOOLEAN);
@@ -18,7 +24,7 @@ static int l_pos (lua_State* L) {
         lua_getfield(L, 1, "x");            // pct | pct.x
         pct.x = lua_tointegerx(L, -1, &ok);
         if (!ok) {
-            luaL_error(L, "expected field 'x'");
+            return luaL_error(L, "expected field 'x'");
         }
         lua_pop(L, 1);                      // pct
     }
@@ -26,7 +32,7 @@ static int l_pos (lua_State* L) {
         lua_getfield(L, 1, "y");            // pct | pct.y
         pct.y = lua_tointegerx(L, -1, &ok);
         if (!ok) {
-            luaL_error(L, "expected field 'y'");
+            return luaL_error(L, "expected field 'y'");
         }
         lua_pop(L, 1);                      // pct
     }
@@ -41,26 +47,75 @@ static int l_pos (lua_State* L) {
     return 1;                               // pct | [pos]
 }
 
+///////////////////////////////////////////////////////////////////////////////
+
+static int l_set_title (lua_State* L) {
+    const char* title = luaL_checkstring(L, 1);   // title
+    pico_set_title(title);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+static int l_input_delay (lua_State* L) {
+    int ms = luaL_checkinteger(L, 1);       // ms
+    pico_input_delay(ms);
+}
+
 static int l_input_event (lua_State* L) {
+    lua_pushlightuserdata(L, (void*)&KEY);  // evt | K
+    lua_gettable(L, LUA_REGISTRYINDEX);     // evt | G
+    lua_getfield(L, -1, "evts");            // evt | G | evts
+    lua_pushvalue(L, 1);                    // evt | G | evts | id
+    lua_gettable(L, -2);                    // evt | G | evts | num
+    int ok;
+    int evt = lua_tointegerx(L, -1, &ok);
+    if (!ok) {
+        return luaL_error(L, "invalid event");
+    }
     Pico_Event e;
-    pico_input_event(&e, PICO_ANY);
+    pico_input_event(&e, evt);
     return 0;
 }
 
-static const luaL_Reg all[] = {
+///////////////////////////////////////////////////////////////////////////////
+
+static const luaL_Reg ll_all[] = {
     { "init", l_init },
     { "pos",  l_pos  },
     { NULL,   NULL }
 };
 
-static const luaL_Reg input[] = {
-    { "event", l_input_event  },
+static const luaL_Reg ll_set[] = {
+    { "title", l_set_title },
+    { NULL,    NULL }
+};
+
+static const luaL_Reg ll_input[] = {
+    { "delay", l_input_delay },
+    { "event", l_input_event },
     { NULL,    NULL }
 };
 
 int luaopen_pico (lua_State* L) {
-    luaL_newlib(L, all);            // pico
-    luaL_newlib(L, input);          // pico | input
-    lua_setfield(L, -2, "input");   // pico
-    return 1;                       // [pico]
+    lua_pushlightuserdata(L, (void*)&KEY);  // K
+    lua_newtable(L);                        // K | G
+    lua_settable(L, LUA_REGISTRYINDEX);     // -
+
+    luaL_newlib(L, ll_all);                 // pico
+    luaL_newlib(L, ll_set);                 // pico | set
+    lua_setfield(L, -2, "set");             // pico
+    luaL_newlib(L, ll_input);               // pico | input
+    lua_setfield(L, -2, "input");           // pico
+
+    {                                       // pico
+        lua_pushlightuserdata(L, (void*)&KEY); // ... | K
+        lua_gettable(L, LUA_REGISTRYINDEX); // ... | G
+        lua_newtable(L);                    // ... | G | evts
+        lua_pushinteger(L, PICO_KEYUP);     // ... | G | evts | UP
+        lua_setfield(L, -2, "pico.key.up"); // ... | G | evts
+        lua_setfield(L, -2, "evts");        // ... | G
+        lua_pop(L, 1);                      // ...
+    }                                       // pico
+
+    return 1;                               // [pico]
 }
