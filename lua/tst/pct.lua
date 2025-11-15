209c5d8761013a51955d9366efe8f8bad37547e9
Francisco Sant'Anna <francisco.santanna@gmail.com>
2025-11-14 21:44:33 -0300

    ...

--- lua/tst/pct.lua
+++ lua/tst/pct.lua
@@ -34,92 +34,95 @@ do
     --_pico_check("pct_rect30_inner50")
 end
 
---[[
-{
+do
     print("rect at 50% anchored by bottom-right")
-    pico_output_clear()
+    pico.output.clear()
 
-    Pico_Pos  pt1 = pico_pos((Pico_Pct){50, 50})
-    Pico_Rect rct1 = {pt1.x, pt1.y, 32, 18}
-    pico_set_anchor_draw((Pico_Anchor){PICO_RIGHT, PICO_BOTTOM})
-    pico_set_color_draw((Pico_Color){0xFF,0xFF,0xFF,0xFF})
-    pico_output_draw_rect(rct1)
+    local pt1  = pico.pos(50, 50)
+    local rct1 = {x=pt1.x, y=pt1.y, w=32, h=18}
+    pico.set.anchor.draw('right', 'bottom')
+    pico.set.color.draw(0xFF,0xFF,0xFF)
+    pico.output.draw.rect(rct1)
 
     print("red anchored by top-left under 0% of white")
-    Pico_Pos pt2 = pico_pos_ext(rct1, (Pico_Pct){0, 0})
-    Pico_Rect rct2 = (Pico_Rect) {pt2.x, pt2.y, 16, 9}
-    pico_set_anchor_draw((Pico_Anchor){PICO_LEFT, PICO_TOP})
-    pico_set_color_draw((Pico_Color){0xFF,0x00,0x00,0xFF})
-    pico_output_draw_rect(rct2)
+    local pt2  = pico.pos({x=0,y=0}, rct1)
+    local rct2 = {x=pt2.x, y=pt2.y, w=16, h=9}
+    pico.set.anchor.draw('left', 'top')
+    pico.set.color.draw(0xFF,0x00,0x00)
+    pico.output.draw.rect(rct2)
 
-_pico_check("pct_rect50_inner0")
-}
+    pico.input.event('key.dn')
+    --_pico_check("pct_rect50_inner0")
+end
 
-{
+do
     print("rect at -10/-10 top-left (4x7 rect on top)")
-    pico_output_clear()
+    pico.output.clear()
 
-    Pico_Pos  pt = pico_pos((Pico_Pct){-10, -10})
-    Pico_Rect rct = {pt.x, pt.y, 10, 10}
-    pico_set_anchor_draw((Pico_Anchor){PICO_LEFT, PICO_TOP})
-    pico_set_color_draw((Pico_Color){0xFF,0xFF,0xFF,0xFF})
-    pico_output_draw_rect(rct)
+    local pt  = pico.pos(-10, -10)
+    local rct = {x=pt.x, y=pt.y, w=10, h=10}
+    pico.set.anchor.draw('left', 'top')
+    pico.set.color.draw(0xFF,0xFF,0xFF)
+    pico.output.draw.rect(rct)
 
-_pico_check("pct_rect-10")
-}
+    pico.input.event('key.dn')
+    --_pico_check("pct_rect-10")
+end
 
-{
+do
     print("rect at 110/110 bottom-right (symmetric to previous)")
-    pico_output_clear()
+    pico.output.clear()
 
-    Pico_Pos  pt = pico_pos((Pico_Pct){110, 110})
-    Pico_Rect rct = {pt.x, pt.y, 10, 10}
-    pico_set_anchor_draw((Pico_Anchor){PICO_RIGHT, PICO_BOTTOM})
-    pico_set_color_draw((Pico_Color){0xFF,0xFF,0xFF,0xFF})
-    pico_output_draw_rect(rct)
+    local pt  = pico.pos(110, 110)
+    local rct = {x=pt.x, y=pt.y, w=10, h=10}
+    pico.set.anchor.draw('right', 'bottom')
+    pico.set.color.draw(0xFF,0xFF,0xFF)
+    pico.output.draw.rect(rct)
 
-_pico_check("pct_rect110")
-}
+    pico.input.event('key.dn')
+    --_pico_check("pct_rect110")
+end
 
-{
+do
     print("rect at 50% anchored by bottom-right")
-    pico_output_clear()
+    pico.output.clear()
 
-    Pico_Pos  pt1 = pico_pos((Pico_Pct){50, 50})
-    Pico_Rect rct1 = {pt1.x, pt1.y, 32, 18}
-    pico_set_anchor_draw((Pico_Anchor){PICO_CENTER, PICO_MIDDLE})
-    pico_set_color_draw((Pico_Color){0xFF,0xFF,0xFF,0xFF})
-    pico_output_draw_rect(rct1)
+    local pt1  = pico.pos(50, 50)
+    local rct1 = {x=pt1.x, y=pt1.y, w=32, h=18}
+    pico.set.anchor.draw('center', 'middle')
+    pico.set.color.draw(0xFF,0xFF,0xFF)
+    pico.output.draw.rect(rct1)
 
     print("red anchored by top-left under -10% of white")
-    Pico_Pos  pt2 = pico_pos_ext(rct1, (Pico_Pct){-10, -10})
-    Pico_Rect rct2 = (Pico_Rect) {pt2.x, pt2.y, 16, 9}
-    pico_set_anchor_draw((Pico_Anchor){PICO_LEFT, PICO_TOP})
-    pico_set_color_draw((Pico_Color){0xFF,0x00,0x00,0xFF})
-    pico_output_draw_rect(rct2)
+    local pt2  = pico.pos(-10, -10, rct1)
+    local rct2 = {x=pt2.x, y=pt2.y, w=16, h=9}
+    pico.set.anchor.draw('left', 'top')
+    pico.set.color.draw(0xFF,0x00,0x00)
+    pico.output.draw.rect(rct2)
 
-_pico_check("pct_rect50_inner-10")
-}
+    pico.input.event('key.dn')
+    --_pico_check("pct_rect50_inner-10")
+end
 
-{
+do
     print("rect at 50%")
-    pico_output_clear()
+    pico.output.clear()
 
-    Pico_Pos  pt1 = pico_pos((Pico_Pct){50, 50})
-    Pico_Rect rct1 = {pt1.x, pt1.y, 32, 18}
-    pico_set_anchor_draw((Pico_Anchor){PICO_CENTER, PICO_MIDDLE})
-    pico_set_color_draw((Pico_Color){0xFF,0xFF,0xFF,0xFF})
-    pico_output_draw_rect(rct1)
+    local pt1  = pico.pos(50, 50)
+    local rct1 = {x=pt1.x, y=pt1.y, w=32, h=18}
+    pico.set.anchor.draw('center', 'middle')
+    pico.set.color.draw(0xFF,0xFF,0xFF)
+    pico.output.draw.rect(rct1)
 
     print("red anchored by top-left under 110% of white (symmetric to previous)")
-    Pico_Pos  pt2 = pico_pos_ext(rct1, (Pico_Pct){110, 110})
-    Pico_Rect rct2 = (Pico_Rect) {pt2.x, pt2.y, 16, 9}
-    pico_set_anchor_draw((Pico_Anchor){PICO_RIGHT, PICO_BOTTOM})
-    pico_set_color_draw((Pico_Color){0xFF,0x00,0x00,0xFF})
-    pico_output_draw_rect(rct2)
+    local pt2  = pico.pos({x=110,y=110}, rct1)
+    local rct2 = {x=pt2.x, y=pt2.y, w=16, h=9}
+    pico.set.anchor.draw('right', 'bottom')
+    pico.set.color.draw(0xFF,0x00,0x00)
+    pico.output.draw.rect(rct2)
 
-_pico_check("pct_rect50_inner110")
-}
-]]
+    pico.input.event('key.dn')
+    --_pico_check("pct_rect50_inner110")
+end
 
 pico.init(false)
