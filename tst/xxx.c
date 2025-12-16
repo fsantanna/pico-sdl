48c77d8baed4260b9356548c4a3fad3bec3f84a1
Francisco Sant'Anna <francisco.santanna@gmail.com>
2025-12-16 11:39:18 -0300

    ...

--- tst/xxx.c
+++ tst/xxx.c
@@ -33,37 +33,24 @@ Pico_Ctx _ctx = {
 
 Pico_Ctx* CTX = &_ctx;
 
-static void _pico_output_present (int force, Pico_Ctx* ctx) {
-    SDL_Rect clip;
-    SDL_RenderGetClipRect(REN, &clip);
-
+static void _pico_output_present (Pico_Ctx* ctx) {
     SDL_Texture* up = (ctx == &_ctx) ? NULL : _ctx.tex;
-
     SDL_SetRenderTarget(REN, up);
     SDL_RenderSetLogicalSize(REN, _ctx.dim.phy.x, _ctx.dim.phy.y);
-
     SDL_Rect dst = { ctx->pos.x, ctx->pos.y, ctx->dim.phy.x, ctx->dim.phy.y };
     SDL_SetTextureAlphaMod(ctx->tex, ctx->alpha);
 printf(">>> [%d] %d\n", ctx==&_ctx, ctx->alpha);
     SDL_RenderCopy(REN, ctx->tex, NULL, &dst);
     //SDL_SetTextureAlphaMod(ctx->tex, 0xFF);
-
     if (ctx == &_ctx) {
         SDL_RenderPresent(REN);
     }
-
     if (ctx != &_ctx) {
-        _pico_output_present(force, &_ctx);
+        _pico_output_present(&_ctx);
     }
-
     Pico_Dim Z = ctx->dim.log;
     SDL_RenderSetLogicalSize(REN, Z.x, Z.y);
     SDL_SetRenderTarget(REN, ctx->tex);
-    SDL_RenderSetClipRect(REN, &clip);
-}
-
-void pico_output_present (void) {
-    _pico_output_present(1, &_ctx);
 }
 
 void pico_set_zoom () {
@@ -82,11 +69,6 @@ void pico_set_zoom () {
     SDL_SetTextureBlendMode(CTX->tex, SDL_BLENDMODE_BLEND);
     SDL_RenderSetLogicalSize(REN, new.x, new.y);
     SDL_SetRenderTarget(REN, CTX->tex);
-
-    // TODO: need to init w/ explicit SetClip to save w/h
-    //       do not pass NULL, GetClip would also return w=0,h=0
-    SDL_Rect clip = { 0, 0, new.x, new.y };
-    SDL_RenderSetClipRect(REN, &clip);
 }
 
 int main (void) {
@@ -107,7 +89,7 @@ int main (void) {
     puts("rect pos=30, dim=50");
     SDL_SetRenderDrawColor(REN, 0x00, 0x00, 0x00, 0xFF);
     SDL_RenderClear(REN);
-    _pico_output_present(0, CTX);
+    _pico_output_present(CTX);
     Pico_Rect r1 = { 192,108,320,180 };
 
     Pico_Ctx ctx = {
@@ -125,7 +107,7 @@ int main (void) {
     CTX->pos = (Pico_Pos) { 32, 18 };
     SDL_SetRenderDrawColor(REN, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderClear(REN);
-    _pico_output_present(0, CTX);
+    _pico_output_present(CTX);
     SDL_Delay(500);
 
     puts("red centered under white");
@@ -134,7 +116,7 @@ int main (void) {
     SDL_SetRenderDrawColor(REN, 0xFF, 0x00, 0x00, 0xFF);
 
     SDL_RenderFillRect(REN, &r2);
-    _pico_output_present(0, CTX);
+    _pico_output_present(CTX);
 
     SDL_Delay(500);
 
@@ -142,16 +124,16 @@ int main (void) {
 
     SDL_SetRenderDrawColor(REN, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderClear(REN);
-    _pico_output_present(0, CTX);
+    _pico_output_present(CTX);
 
     CTX = &ctx;
 
     CTX->alpha = 0x88;
-    _pico_output_present(0, CTX);
+    _pico_output_present(CTX);
     SDL_Delay(500);
-    _pico_output_present(0, CTX);
+    _pico_output_present(CTX);
     SDL_Delay(500);
-    _pico_output_present(0, CTX);
+    _pico_output_present(CTX);
     SDL_Delay(500);
 
     return 0;
