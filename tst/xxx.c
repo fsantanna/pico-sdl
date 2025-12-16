eaf640c89de54d671ad04d2e296131578252a446
Francisco Sant'Anna <francisco.santanna@gmail.com>
2025-12-16 12:49:19 -0300

    ...

--- tst/xxx.c
+++ tst/xxx.c
@@ -4,30 +4,18 @@
 static SDL_Window*  WIN;
 static SDL_Texture* TEX;
 static SDL_Texture* tex;
-static int alpha;
 
 #define REN (SDL_GetRenderer(WIN))
-
-static void out0 () {
-    SDL_SetRenderTarget(REN, NULL);
-    SDL_RenderCopy(REN, TEX, NULL, NULL);
-    SDL_RenderPresent(REN);
-    SDL_SetRenderTarget(REN, TEX);
-}
 
 static void out1 () {
     SDL_SetRenderTarget(REN, TEX);
     SDL_Rect dst = { 32, 18, 320, 180 };
-    SDL_SetTextureAlphaMod(tex, alpha);
-printf(">>> [%d] %d\n", 1, alpha);
     SDL_RenderCopy(REN, tex, NULL, &dst);
-    //SDL_SetTextureAlphaMod(tex, 0xFF);
 
     {
         SDL_SetRenderTarget(REN, NULL);
         SDL_RenderCopy(REN, TEX, NULL, NULL);
         SDL_RenderPresent(REN);
-        SDL_SetRenderTarget(REN, TEX);
     }
 
     SDL_SetRenderTarget(REN, tex);
@@ -51,7 +39,12 @@ int main (void) {
 
     SDL_SetRenderDrawColor(REN, 0x00, 0x00, 0x00, 0xFF);
     SDL_RenderClear(REN);
-    out0();
+    {
+        SDL_SetRenderTarget(REN, NULL);
+        SDL_RenderCopy(REN, TEX, NULL, NULL);
+        SDL_RenderPresent(REN);
+        SDL_SetRenderTarget(REN, TEX);
+    }
 
     tex = SDL_CreateTexture (
         REN, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_TARGET,
@@ -69,16 +62,21 @@ int main (void) {
 
     SDL_SetRenderDrawColor(REN, 0xFF, 0x00, 0x00, 0xFF);
     SDL_RenderFillRect(REN, &r2);
-    alpha = 0xFF;
+    SDL_SetTextureAlphaMod(tex, 0xFF);
     out1();
 
     SDL_Delay(250);
 
     SDL_SetRenderDrawColor(REN, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderClear(REN);
-    out0();
+    {
+        SDL_SetRenderTarget(REN, NULL);
+        SDL_RenderCopy(REN, TEX, NULL, NULL);
+        SDL_RenderPresent(REN);
+        SDL_SetRenderTarget(REN, TEX);
+    }
 
-    alpha = 0x88;
+    SDL_SetTextureAlphaMod(tex, 0x88);
     out1();
     SDL_Delay(250);
     out1();
