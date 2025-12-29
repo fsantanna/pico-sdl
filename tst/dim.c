7af89ef0e1f7730c34e2e1d6bc5e307113d70ca5
Francisco Sant'Anna <francisco.santanna@gmail.com>
2025-12-29 15:34:30 -0300

    ...

--- tst/dim.c
+++ tst/dim.c
@@ -5,38 +5,37 @@ int main (void) {
     pico_init(1);
     pico_set_title("Dim");
 
-    Pico_Pos p = pico_pos((Pico_Pct){50, 50});
+    Pico_Rect_Pct r = { 0.5,0.5, 0,0, PICO_ANCHOR_C, NULL };
     char fmt[64];
 
-    pico_output_clear();
-    for (int i = 0; i <= 100; i+=25) {
-        Pico_Dim d = pico_dim((Pico_Pct){i, i});
-        pico_output_draw_rect((Pico_Rect){p.x,p.y,d.x,d.y});
-
-        sprintf(fmt, "%d_screen_size", i);
+    for (float i=0; i<=1; i+=0.25) {
+        pico_output_clear();
+        r.w = r.h = i;
+        pico_output_draw_rect_pct(&r);
+        sprintf(fmt, "size: %.2f", i);
         puts(fmt);
-        _pico_check(fmt);
+        //_pico_check(fmt);
+        pico_input_event(NULL, PICO_KEYDOWN);
     }
 
-    for (int i = 0; i <= 120; i+=20) {
+    r.w = r.h = 0.8;
+    for (float i=0; i<=1.25; i+=0.25) {
         pico_output_clear();
-        Pico_Rect r = {p.x,p.y,50,20};
+
         pico_set_alpha(255);
         pico_set_color_draw((Pico_Color){255,255,255});
-        pico_output_draw_rect(r);
+        pico_output_draw_rect_pct(&r);
 
-        Pico_Dim d = pico_dim_ext((Pico_Pct){i, i}, (Pico_Dim){r.w,r.h});
+        Pico_Rect_Pct rr = { 0.5,0.5, i,i, PICO_ANCHOR_C, &r };
         pico_set_alpha(150);
         pico_set_color_draw((Pico_Color){255,0,0});
-        pico_output_draw_rect((Pico_Rect){p.x,p.y,d.x,d.y});
+        pico_output_draw_rect_pct(&rr);
 
-        sprintf(fmt, "%d_rect_size", i);
+        sprintf(fmt, "size: %.2f", i);
         puts(fmt);
-        _pico_check(fmt);
+        //_pico_check(fmt);
+        pico_input_event(NULL, PICO_KEYDOWN);
     }
-
-    puts("ASSERT ERROR EXPECTED:");
-    pico_dim((Pico_Pct){-1, -1});
 
     pico_init(0);
     return 0;
