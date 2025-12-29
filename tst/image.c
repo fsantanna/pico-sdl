4d55177f14d90448c6454398e3270d459daf5c02
Francisco Sant'Anna <francisco.santanna@gmail.com>
2025-12-29 00:19:35 -0300

    ...

--- tst/image.c
+++ tst/image.c
@@ -14,48 +14,60 @@ int main (void) {
         //_pico_check("img_big_center");
         pico_input_event(NULL, PICO_KEYDOWN);
     }
-
     {
         puts("show small centered");
         Pico_Rect r = { 50-5,50-5, 10,10 };
         pico_output_clear();
-        pico_output_draw_image_raw(r,"open.png");
+        pico_output_draw_image_raw(r, "open.png");
         //_pico_check("img_small_center");
         pico_input_event(NULL, PICO_KEYDOWN);
     }
-
+    {
+        puts("show small/medium distorted");
+        Pico_Rect r = { 50-5,50-10, 10,20 };
+        pico_output_clear();
+        pico_output_draw_image_raw(r, "open.png");
+        //_pico_check("img_smallmedium_distorted");
+        pico_input_event(NULL, PICO_KEYDOWN);
+    }
+    {
+        puts("show medium normal");
+        pico_output_clear();
+        Pico_Rect r = { 50-10,50-10, 20,0 };
+        pico_output_draw_image_raw(r, "open.png");
+        //_pico_check("img_medium_normal");
+        pico_input_event(NULL, PICO_KEYDOWN);
+    }
+    {
+        puts("show small normal");
+        pico_output_clear();
+        Pico_Rect r = { 50-5,50-5, 0,10 };
+        pico_output_draw_image_raw(r, "open.png");
+        //_pico_check("img_small_normal");
+        pico_input_event(NULL, PICO_KEYDOWN);
+    }
+    {
+        puts("show big centered");
+        pico_output_clear();
+        Pico_Rect r = { 50-24,50-24, 0,0 };
+        pico_output_draw_image_raw(r, "open.png");
+        _pico_check("img_big_center2");
+    }
 #if 0
-    pico_output_clear();
-    pico_output_draw_image_ext(cnt,"open.png",(Pico_Dim){10,15});
-    puts("show small/medium distorted");
-    _pico_check("img_smallmedium_distorted");
-
-    pico_output_clear();
-    pico_output_draw_image_ext(cnt,"open.png",(Pico_Dim){15,0});
-    puts("show medium normal");
-    _pico_check("img_medium_normal");
-
-    pico_output_clear();
-    pico_output_draw_image_ext(cnt,"open.png",(Pico_Pos){0,10});
-    puts("show small normal");
-    _pico_check("img_small_normal");
-
-    pico_output_clear();
-    pico_output_draw_image_ext(cnt,"open.png",(Pico_Dim){0,0});
-    puts("show big centered");
-    _pico_check("img_big_center2");
-
-    pico_output_clear();
-    pico_set_crop((Pico_Rect){9,9,30,30});
-    pico_output_draw_image_ext(cnt,"open.png",(Pico_Dim){30,30});
-    puts("show big croped");
-    _pico_check("img_big_cropped");
-
-    pico_output_clear();
-    pico_set_crop((Pico_Rect){0,0,0,0});
-    pico_output_draw_image_ext(cnt,"open.png",(Pico_Dim){0,15});
-    puts("show medium normal");
-    _pico_check("img_medium_normal2");
+    {
+        pico_output_clear();
+        pico_set_crop((Pico_Rect){9,9,30,30});
+        pico_output_draw_image_ext(cnt,"open.png",(Pico_Dim){30,30});
+        puts("show big croped");
+        _pico_check("img_big_cropped");
+    }
+    {
+        pico_output_clear();
+        pico_set_crop((Pico_Rect){0,0,0,0});
+        pico_output_draw_image_ext(cnt,"open.png",(Pico_Dim){0,15});
+        puts("show medium normal");
+        _pico_check("img_medium_normal2");
+    }
 #endif
 
     pico_init(0);
