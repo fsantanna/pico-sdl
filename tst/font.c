166c3d5735cb6710101217d14a1802be3dfefbdc
Francisco Sant'Anna <francisco.santanna@gmail.com>
2025-12-29 16:09:35 -0300

    - TODO: font

--- tst/font.c
+++ tst/font.c
@@ -2,9 +2,18 @@
 
 int main (void) {
     pico_init(1);
-    Pico_Pos pt = pico_pos((Pico_Pct){50, 50});
-    pico_output_draw_text(pt, "Hello!");
-    pico_input_delay(1000);
+
+    {
+        Pico_Rect r = { 50,50, 0,10 };
+        pico_output_draw_text_raw(r, "Hello!");
+        pico_input_delay(1000);
+    }
+    {
+        Pico_Rect_Pct r = { 0.5,0.5, 0,0.2, PICO_ANCHOR_C, NULL };
+        pico_output_draw_text_pct(&r, "Hello!");
+        pico_input_delay(1000);
+    }
+
     pico_init(0);
     return 0;
 }
