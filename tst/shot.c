fe47ae181dd8bc5677683a9b3cd46da63de9f877
Francisco <francisco.santanna@gmail.com>
2026-01-12 17:08:43 -0300

    - TODO: tst/shot.c

--- tst/shot.c
+++ tst/shot.c
@@ -7,34 +7,24 @@ int main (void) {
     puts("entire screen");
     pico_output_draw_rect_raw((Pico_Rect){10, 10, 20, 20});
     assert(pico_output_screenshot(NULL) != NULL);
-    pico_input_event(NULL, PICO_KEYDOWN);
 
-    puts("entire screen - second.png");
-    pico_set_color_draw((Pico_Color){200,0,0});
+    puts("entire screen - shot-01.png");
+    pico_set_color_draw((Pico_Color){200, 0, 0});
     pico_output_draw_rect_raw((Pico_Rect){30, 30, 10, 10});
-    assert(!strcmp(pico_output_screenshot("gen/shot-01.png"),"asr/shot-01.png"));
-    pico_input_event(NULL, PICO_KEYDOWN);
+    assert(!strcmp(pico_output_screenshot("gen/shot-01.png"), "gen/shot-01.png"));
 
-    puts("part of screen");
-    pico_set_color_draw((Pico_Color){0,200,0});
+    puts("part of screen (raw)");
+    pico_set_color_draw((Pico_Color){0, 200, 0});
     pico_output_draw_rect_raw((Pico_Rect){40, 5, 10, 10});
-    assert(pico_output_screenshot_ext(NULL, (Pico_Rect){0, 0, 50, 30}) != NULL);
-    pico_input_event(NULL, PICO_KEYDOWN);
+    assert(pico_output_screenshot_raw(NULL, (Pico_Rect){0, 0, 250, 150}) != NULL);
 
-    pico_set_zoom((Pico_Dim){200, 200});
-
-    puts("zoomed screen");
-    pico_set_color_draw((Pico_Color){0,200,0});
-    pico_output_draw_rect_raw((Pico_Rect){40, 5, 10, 10});
-    assert(pico_output_screenshot(NULL) != NULL);
-    pico_input_event(NULL, PICO_KEYDOWN);
-
-    puts("part of zoomed screen");
-    pico_set_color_draw((Pico_Color){0,200,0});
-    pico_output_draw_rect_raw((Pico_Rect){0, 0, 10, 10});
-    assert(pico_output_screenshot_ext(NULL, (Pico_Rect){0, 0, 10, 10}) != NULL);
-    pico_input_event(NULL, PICO_KEYDOWN);
+    puts("part of screen (pct)");
+    pico_set_color_draw((Pico_Color){0, 0, 200});
+    pico_output_draw_rect_raw((Pico_Rect){50, 50, 10, 10});
+    assert(pico_output_screenshot_pct(NULL,
+        &(Pico_Rect_Pct){0, 0, 0.5, 0.3, {0, 0}, NULL}) != NULL);
 
     pico_init(0);
+    puts("All tests passed!");
     return 0;
 }
