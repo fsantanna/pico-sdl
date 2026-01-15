754e88a2f87151c1a440bc1941111df67a925db9
Francisco <francisco.santanna@gmail.com>
2026-01-15 13:13:43 -0300

    - TODO: tst/navigate

--- tst/todo_navigate.c
+++ tst/todo_navigate.c
@@ -1,10 +1,21 @@
 #include "pico.h"
+#include <SDL2/SDL.h>
+
+// Push a Ctrl+key event to simulate navigation
+void push_key (SDL_Keycode key) {
+    SDL_Event e = {0};
+    e.type = SDL_KEYDOWN;
+    e.key.keysym.sym = key;
+    e.key.keysym.mod = KMOD_LCTRL;
+    SDL_PushEvent(&e);
+}
 
 int main() {
     pico_init(1);
     pico_set_view_raw(-1, NULL, NULL, &(Pico_Dim){1000, 1000}, NULL, NULL);
 
-    while (1) {
+    // draw
+    {
         // Draw X at center of world
         pico_output_draw_line_pct(
             &(Pico_Pos_Pct){0.45, 0.45, PICO_ANCHOR_C, NULL},
@@ -20,9 +31,18 @@ int main() {
         pico_output_draw_rect_pct(
             &(Pico_Rect_Pct){0.5, 0.5, 0.25, 0.25, PICO_ANCHOR_C, NULL}
         );
-
-        pico_input_event(NULL, PICO_ANY);
     }
+
+    // Generate navigation key presses: 5 up, 3 left, 2 right, 4 down
+    _pico_check("navigate-01");
+    for (int i=0; i<5; i++) push_key(SDLK_UP);
+    _pico_check("navigate-02");
+    for (int i=0; i<3; i++) push_key(SDLK_LEFT);
+    _pico_check("navigate-03");
+    for (int i=0; i<2; i++) push_key(SDLK_RIGHT);
+    _pico_check("navigate-04");
+    for (int i=0; i<4; i++) push_key(SDLK_DOWN);
+    _pico_check("navigate-05");
 
     pico_init(0);
     return 0;
