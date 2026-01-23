d78513fe3bf16640cbae9fcc67fc18753975fa6e
Francisco <francisco.santanna@gmail.com>
2026-01-23 16:23:20 -0300

    - tst/anchor -> raw/pct

---
+++ tst/anchor_raw.c
@@ -0,0 +1,97 @@
+#include "pico.h"
+#include "../check.h"
+
+int main (void) {
+    pico_init(1);
+    pico_set_title("Anchoring Raw...");
+
+    Pico_Dim phy = { '!', {200,200}, NULL };
+    Pico_Dim log = { '!', { 10, 10}, NULL };
+    pico_set_view(-1, -1, &phy, NULL, &log, NULL, NULL);
+
+    // PIXELS
+    {
+        puts("centered pixel - 1dir/1baixo");
+        Pico_Pos p = { '!', {5,5}, PICO_ANCHOR_C, NULL };
+        pico_output_clear();
+        pico_output_draw_pixel(&p);
+        _pico_check("anchor_raw-01");
+    }
+
+    {
+        puts("centered pixel - 1dir/1baixo");
+        Pico_Pos p = { '!', {5,5}, PICO_ANCHOR_NW, NULL };
+        pico_output_clear();
+        pico_output_draw_pixel(&p);
+        _pico_check("anchor_raw-02");
+    }
+
+    {
+        puts("centered pixel - 1esq/1cima");
+        Pico_Pos p = { '!', {5,5}, PICO_ANCHOR_SE, NULL };
+        pico_output_clear();
+        pico_output_draw_pixel(&p);
+        _pico_check("anchor_raw-03");
+    }
+
+    // RECTS
+    {
+        puts("centered rect - exact");
+        Pico_Rect r = { '!', {5, 5, 4, 4}, PICO_ANCHOR_C, NULL };
+        pico_output_clear();
+        pico_output_draw_rect(&r);
+        _pico_check("anchor_raw-04");
+    }
+
+    {
+        puts("topleft centered - 1lin/1col");
+        Pico_Rect r = { '!', {5, 5, 4, 4}, PICO_ANCHOR_NW, NULL };
+        pico_output_clear();
+        pico_output_draw_rect(&r);
+        _pico_check("anchor_raw-05");
+    }
+
+    {
+        puts("bottomright centered - 1lin/1col");
+        Pico_Rect r = { '!', {5, 5, 4, 4}, PICO_ANCHOR_SE, NULL };
+        pico_output_clear();
+        pico_output_draw_rect(&r);
+        _pico_check("anchor_raw-06");
+    }
+
+    {
+        puts("rightmiddle centered - 1col");
+        Pico_Rect r = { '!', {5, 5, 4, 4}, PICO_ANCHOR_E, NULL };
+        pico_output_clear();
+        pico_output_draw_rect(&r);
+        _pico_check("anchor_raw-07");
+    }
+
+    {
+        puts("anchor 25%25% 20%20% - 1lin/1col");
+        Pico_Rect r = { '!', {2, 2, 4, 4}, {.x=0.2,.y=0.2}, NULL };
+        pico_output_clear();
+        pico_output_draw_rect(&r);
+        _pico_check("anchor_raw-08");
+    }
+
+    // RECTS out of [0,10]
+    {
+        puts("anchor -25%-25% centered - touching border");
+        Pico_Rect r = { '!', {5, 5, 4, 4}, {.x=-0.25,.y=-0.25}, NULL };
+        pico_output_clear();
+        pico_output_draw_rect(&r);
+        _pico_check("anchor_raw-09");
+    }
+
+    {
+        puts("anchor 125%125% centered - touching border");
+        Pico_Rect r = { '!', {5, 5, 4, 4}, {.x=1.25,.y=1.25}, NULL };
+        pico_output_clear();
+        pico_output_draw_rect(&r);
+        _pico_check("anchor_raw-10");
+    }
+
+    pico_init(0);
+    return 0;
+}
