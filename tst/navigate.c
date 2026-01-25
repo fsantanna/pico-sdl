aeb7120ed5b58e6682f7ee0b6244b5438c6dda09
Francisco Sant'Anna <francisco.santanna@gmail.com>
2026-01-25 07:45:53 -0300

    - tst/navigate.c

--- tst/navigate.c
+++ tst/navigate.c
@@ -14,24 +14,25 @@ void push_key (SDL_Keycode key) {
 
 int main() {
     pico_init(1);
-    pico_set_view_raw(-1, -1, NULL, NULL, &(Pico_Dim){1000, 1000}, NULL, NULL);
+    pico_set_view(-1, -1, NULL, NULL,
+        &(Pico_Rel_Dim){ '!', {1000, 1000}, NULL }, NULL, NULL);
 
     // draw
     {
         // X at center
-        pico_output_draw_line_pct(
-            &(Pico_Pos_Pct){0.45, 0.45, PICO_ANCHOR_C, NULL},
-            &(Pico_Pos_Pct){0.55, 0.55, PICO_ANCHOR_C, NULL}
+        pico_output_draw_line(
+            &(Pico_Rel_Pos){ '%', {0.45, 0.45}, PICO_ANCHOR_C, NULL },
+            &(Pico_Rel_Pos){ '%', {0.55, 0.55}, PICO_ANCHOR_C, NULL }
         );
-        pico_output_draw_line_pct(
-            &(Pico_Pos_Pct){0.55, 0.45, PICO_ANCHOR_C, NULL},
-            &(Pico_Pos_Pct){0.45, 0.55, PICO_ANCHOR_C, NULL}
+        pico_output_draw_line(
+            &(Pico_Rel_Pos){ '%', {0.55, 0.45}, PICO_ANCHOR_C, NULL },
+            &(Pico_Rel_Pos){ '%', {0.45, 0.55}, PICO_ANCHOR_C, NULL }
         );
 
         // RECT at center
         pico_set_style(PICO_STYLE_STROKE);
-        pico_output_draw_rect_pct(
-            &(Pico_Rect_Pct){0.5, 0.5, 0.25, 0.25, PICO_ANCHOR_C, NULL}
+        pico_output_draw_rect(
+            &(Pico_Rel_Rect){ '%', {0.5, 0.5, 0.25, 0.25}, PICO_ANCHOR_C, NULL }
         );
     }
 
--- tst/todo_control.c
+++ tst/todo_control.c
@@ -2,9 +2,9 @@
 
 int main (void) {
     pico_init(1);
-    Pico_Rect_Pct r = { 0.5,0.5, 0,0.2, PICO_ANCHOR_C, NULL };
+    Pico_Rel_Rect r = { '%', {0.5,0.5, 0,0.2}, PICO_ANCHOR_C, NULL };
     while (1) {
-        pico_output_draw_text_pct("Hello!", &r);
+        pico_output_draw_text("Hello!", &r);
         pico_input_event(NULL, PICO_ANY);
         pico_output_clear();
     }
