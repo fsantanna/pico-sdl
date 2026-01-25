6aedf2f06ed1d81b5e767f03b50c56fea398c210
Francisco Sant'Anna <francisco.santanna@gmail.com>
2026-01-24 21:57:37 -0300

    - tst/clip_pct.c

--- tst/clip_pct.c
+++ tst/clip_pct.c
@@ -7,31 +7,31 @@ int main (void) {
 
     puts("rect at 30%");
     pico_output_clear();
-    Pico_Rel_Rect r1 = { 0.3,0.3, 0.5,0.5, PICO_ANCHOR_C, NULL };
-    pico_output_draw_rect_pct(&r1);
+    Pico_Rel_Rect r1 = { '%', {0.3,0.3, 0.5,0.5}, PICO_ANCHOR_C, NULL };
+    pico_output_draw_rect(&r1);
     _pico_check("clip-01");     // same as raw
 
-    pico_set_view_pct(-1, -1, NULL, NULL, NULL, NULL, &r1);
+    pico_set_view(-1, -1, NULL, NULL, NULL, NULL, &r1);
     pico_set_color_clear((Pico_Color){0xCC, 0xCC, 0xCC});
     pico_output_clear();
     _pico_check("clip-02");     // same as raw
 
     puts("red centered under white");
-    Pico_Rel_Rect r2 = { 0.5,0.5, 0.5,0.5, PICO_ANCHOR_C, &r1 };
+    Pico_Rel_Rect r2 = { '%', {0.5,0.5, 0.5,0.5}, PICO_ANCHOR_C, &r1 };
     pico_set_color_draw((Pico_Color){0xFF,0x00,0x00});
-    pico_output_draw_rect_pct(&r2);
+    pico_output_draw_rect(&r2);
     _pico_check("clip-03");     // same as raw
 
     puts("blue centered under white");
-    Pico_Rel_Pos_Pct pt3 = { 0.5,0.5, PICO_ANCHOR_C, &r1 };
+    Pico_Rel_Pos pt3 = { '%', {0.5,0.5}, PICO_ANCHOR_C, &r1 };
     pico_set_color_draw((Pico_Color){0x00,0x00,0xFF});
-    pico_output_draw_pixel_pct(&pt3);
+    pico_output_draw_pixel(&pt3);
     _pico_check("clip-04");     // same as raw
 
     puts("yellow clipped under white bottom right");
-    Pico_Rel_Rect r4 = { 1,1, 0.5,0.5, PICO_ANCHOR_C, &r1 };
+    Pico_Rel_Rect r4 = { '%', {1,1, 0.5,0.5}, PICO_ANCHOR_C, &r1 };
     pico_set_color_draw((Pico_Color){0xFF,0xFF,0x00});
-    pico_output_draw_rect_pct(&r4);
+    pico_output_draw_rect(&r4);
     _pico_check("clip-05");     // same as raw
 
     pico_init(0);
