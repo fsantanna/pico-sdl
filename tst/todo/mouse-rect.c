1b0ba5d06a2e371b24471ca3e8f1a9a1c1c92e26
Francisco Sant'Anna <francisco.santanna@gmail.com>
2026-04-02 16:33:49 -0300

    - tst/todo/mouse-rect.c

--- tst/todo/mouse-rect.c
+++ tst/todo/mouse-rect.c
@@ -2,11 +2,27 @@
 
 int main (void) {
     pico_init(1);
-    pico_output_clear();
 
     Pico_Rel_Rect r = { '%', {0.7, 0.3, 0.6, 0.4}, PICO_ANCHOR_C, NULL };
-    Pico_Mouse m = pico_get_mouse_rect(&r);
-    printf(">>> %f %f\n", m.x, m.y);
+
+    /* Draw the rect */
+    pico_set_style(PICO_STYLE_STROKE);
+    pico_output_draw_rect(&r);
+
+    /* Event loop on mouse motion */
+    Pico_Event evt;
+    while (pico_input_event_timeout(&evt, PICO_EVENT_MOUSE_BUTTON_DN, -1)) {
+        if (evt.type == PICO_EVENT_QUIT) {
+            break;
+        }
+        Pico_Mouse win = pico_get_mouse('w', NULL);
+        Pico_Mouse raw = pico_get_mouse('!', &r);
+        Pico_Mouse pct = pico_get_mouse('%', &r);
+        printf (
+            ">>> w %4.0f %4.0f | ! %6.1f %6.1f | %% %5.3f %5.3f\n",
+            win.x, win.y, raw.x, raw.y, pct.x, pct.y
+        );
+    }
 
     pico_init(0);
     return 0;
