b280b8a9cc0e9aa8032deef1692cebf4d40efbcb
Francisco Sant'Anna <francisco.santanna@gmail.com>
2026-04-02 16:40:52 -0300

    ...

--- tst/todo/mouse-rect.c
+++ tst/todo/mouse-rect.c
@@ -3,13 +3,21 @@
 int main (void) {
     pico_init(1);
 
-    Pico_Rel_Rect r = { '%', {0.7, 0.3, 0.6, 0.4}, PICO_ANCHOR_C, NULL };
+    /* Create layer 50x50, draw centered circle */
+    pico_layer_empty("A", (Pico_Abs_Dim){50, 50});
+    pico_set_layer("A");
+    pico_set_color_clear(PICO_COLOR_RED);
+    pico_output_clear();
+    pico_output_draw_oval(&(Pico_Rel_Rect){ '%', {0.3, 0.6, 0.5, 0.5}, PICO_ANCHOR_C, NULL });
+    pico_set_layer(NULL);
 
-    /* Draw the rect */
-    pico_set_style(PICO_STYLE_STROKE);
-    pico_output_draw_rect(&r);
+    /* Draw layer distorted 2:1 on main */
+    Pico_Rel_Rect r = { '%', {0.6, 0.3, 0.8, 0.4}, PICO_ANCHOR_C, NULL };
+    pico_set_color_clear(PICO_COLOR_BLACK);
+    pico_output_clear();
+    pico_output_draw_layer("A", &r);
 
-    /* Event loop on mouse motion */
+    /* Event loop */
     Pico_Event evt;
     while (pico_input_event_timeout(&evt, PICO_EVENT_MOUSE_BUTTON_DN, -1)) {
         if (evt.type == PICO_EVENT_QUIT) {
@@ -18,7 +26,7 @@ int main (void) {
         Pico_Mouse win = pico_get_mouse('w', NULL);
         Pico_Mouse raw = pico_get_mouse('!', &r);
         Pico_Mouse pct = pico_get_mouse('%', &r);
-        printf (
+        printf(
             ">>> w %4.0f %4.0f | ! %6.1f %6.1f | %% %5.3f %5.3f\n",
             win.x, win.y, raw.x, raw.y, pct.x, pct.y
         );
