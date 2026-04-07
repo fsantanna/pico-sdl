098918ab8ce6b26ee18687bd18702f72632b05dc
Francisco Sant'Anna <francisco.santanna@gmail.com>
2026-04-07 19:11:45 -0300

    - TODO: mouse-w-click bug: 1-off collision vs draw

--- tst/mouse-w-click.c
+++ tst/mouse-w-click.c
@@ -16,37 +16,27 @@ int main (void) {
     pico_output_draw_rect(&btn);
     pico_set_layer(NULL);
 
-    /* Composite layer A centered, 50% of screen. */
-    Pico_Rel_Rect r = { '%', {0.5, 0.5, 0.5, 0.5}, PICO_ANCHOR_C, NULL };
+    /* Composite layer A distorted at SE, 35%x35% of screen. */
+    Pico_Rel_Rect r = { '%', {0.99, 0.99, 0.35, 0.35}, PICO_ANCHOR_SE, NULL };
     pico_set_color_clear(PICO_COLOR_BLACK);
     pico_output_clear();
     pico_output_draw_layer("A", &r);
 
-    /* Mouse near visible btn's right edge. */
-
-    pico_set_mouse(&(Pico_Rel_Pos){ 'w', {267, 250}, PICO_ANCHOR_NW, NULL });
-    Pico_Mouse pct1 = pico_get_mouse('%', &r);
-    Pico_Rel_Pos pos1 = { '%', {pct1.x, pct1.y}, PICO_ANCHOR_NW, &r };
-
-    pico_set_mouse(&(Pico_Rel_Pos){ 'w', {268, 250}, PICO_ANCHOR_NW, NULL });
-    Pico_Mouse pct2 = pico_get_mouse('%', &r);
-    Pico_Rel_Pos pos2 = { '%', {pct2.x, pct2.y}, PICO_ANCHOR_NW, &r };
-
     btn.up = &r;
 
-    /* Collision passes against the phantom screen-% btn... */
-    assert(pico_vs_pos_rect(&pos1, &btn));
-    assert(!pico_vs_pos_rect(&pos2, &btn));
+    /* Win pixel (400,382) is inside btn in r-% (continuous), but 'w' draw
+       snaps through the screen log grid (5 win px / log px) to a cell that
+       lies above btn's top edge (381.25 win). Collision and rendering
+       disagree even with btn.up = &r. */
+    pico_set_mouse(&(Pico_Rel_Pos){ 'w', {400, 382}, PICO_ANCHOR_NW, NULL });
+    Pico_Mouse pct = pico_get_mouse('%', &r);
+    Pico_Rel_Pos pos = { '%', {pct.x, pct.y}, PICO_ANCHOR_NW, &r };
+
+    assert(pico_vs_pos_rect(&pos, &btn));
 
     pico_set_color_draw(PICO_COLOR_GREEN);
-    pico_output_draw_pixel(&(Pico_Rel_Pos){ 'w', {267, 250}, PICO_ANCHOR_NW, NULL });
-    pico_output_draw_pixel(&pos1);
+    pico_output_draw_pixel(&(Pico_Rel_Pos){ 'w', {400, 382}, PICO_ANCHOR_NW, NULL });
     _pico_check("mouse-w-click-01");
-
-    pico_set_color_draw(PICO_COLOR_RED);
-    pico_output_draw_pixel(&(Pico_Rel_Pos){ 'w', {268, 250}, PICO_ANCHOR_NW, NULL });
-    pico_output_draw_pixel(&pos2);
-    _pico_check("mouse-w-click-02");
 
     pico_init(0);
     return 0;
