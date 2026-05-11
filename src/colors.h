92f80a9a9fe7cb34f77c724ff171ed5ef07e808c
Francisco Sant'Anna <francisco.santanna@gmail.com>
2026-05-11 16:05:20 -0300

    - colors.h <- pico.c

--- src/colors.h
+++ src/colors.h
@@ -71,4 +71,53 @@ const Pico_Color PICO_COLOR_TRANSPARENT = {0, 0, 0, 0};
 
 /// @}
 
+Pico_Color pico_color_darker (Pico_Color clr, float pct) {
+    if (pct < 0) {
+        return pico_color_lighter(clr, -pct);
+    }
+    float X = MAX(0, 1-pct);
+    return (Pico_Color) { clr.r*X, clr.g*X, clr.b*X, clr.a };
+}
+
+Pico_Color pico_color_lighter (Pico_Color clr, float pct) {
+    if (pct < 0) {
+        return pico_color_darker(clr, -pct);
+    }
+    return (Pico_Color) {
+        (clr.r + (255 - clr.r) * pct),
+        (clr.g + (255 - clr.g) * pct),
+        (clr.b + (255 - clr.b) * pct),
+        clr.a
+    };
+}
+
+Pico_Color pico_color_mix (Pico_Color c1, Pico_Color c2) {
+    return (Pico_Color) {
+        (c1.r + c2.r) / 2,
+        (c1.g + c2.g) / 2,
+        (c1.b + c2.b) / 2,
+        (c1.a + c2.a) / 2
+    };
+}
+
+Pico_Color pico_color_alpha (Pico_Color clr, Uint8 a) {
+    clr.a = a;
+    return clr;
+}
+
+Pico_Color pico_color_hex (uint32_t hex) {
+    if (hex > 0xFFFFFF) {
+        uint8_t r = (hex >> 24) & 0xFF;
+        uint8_t g = (hex >> 16) & 0xFF;
+        uint8_t b = (hex >> 8)  & 0xFF;
+        uint8_t a =  hex        & 0xFF;
+        return (Pico_Color) { r, g, b, a };
+    } else {
+        uint8_t r = (hex >> 16) & 0xFF;
+        uint8_t g = (hex >> 8)  & 0xFF;
+        uint8_t b =  hex        & 0xFF;
+        return (Pico_Color) { r, g, b, 0xFF };
+    }
+}
+
 #endif // PICO_COLORS_C
