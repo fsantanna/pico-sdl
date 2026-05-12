0e635688142478715de89b65a1ea00a7d40ff9be
Francisco Sant'Anna <francisco.santanna@gmail.com>
2026-05-11 22:11:37 -0300

    - tst/in.{c,lua} simplified

--- lua/tst/in.lua
+++ lua/tst/in.lua
@@ -1,81 +1,39 @@
 pico.init(true)
 pico.set.scene { dim = {'!', w=100, h=100} }
 
-print "pico.xin.rect - ! NW"
-do
-    local out   = {'!', x=20, y=20, w=60, h=60, anchor='NW'}
-    local child = {'!', x=10, y=10, w=20, h=20, anchor='NW'}
-    local ret = pico.xin.rect(out, child)
-    local abs = pico.cv.rect(ret)
-    assert(abs.x==30 and abs.y==30 and abs.w==20 and abs.h==20)
-    assert(ret[1] == '!')
-end
-
-print "pico.xin.rect - % C in ! NW"
-do
-    local out   = {'!', x=20, y=20, w=60, h=60, anchor='NW'}
-    local child = {'%', x=0.5, y=0.5, w=0.5, h=0.5, anchor='C'}
-    local ret = pico.xin.rect(out, child)
-    local abs = pico.cv.rect(ret)
-    assert(abs.x==35 and abs.y==35 and abs.w==30 and abs.h==30)
-    assert(ret[1] == '%')
-end
-
-print "pico.xin.pos - ! NW"
-do
-    local out   = {'!', x=20, y=20, w=60, h=60, anchor='NW'}
-    local child = {'!', x=10, y=10, anchor='NW'}
-    local ret = pico.xin.pos(out, child)
-    local abs = pico.cv.pos(ret)
-    assert(abs.x==30 and abs.y==30)
-end
-
-print "pico.xin.pos - % NW"
-do
-    local out   = {'!', x=20, y=20, w=60, h=60, anchor='NW'}
-    local child = {'%', x=0.5, y=0.5, anchor='NW'}
-    local ret = pico.xin.pos(out, child)
-    local abs = pico.cv.pos(ret)
-    assert(abs.x==50 and abs.y==50)
-end
-
-print "pico.xin.dim - !"
-do
-    local out   = {'!', x=20, y=20, w=60, h=60, anchor='NW'}
-    local child = {'!', w=30, h=20}
-    local ret = pico.xin.dim(out, child)
-    local abs = pico.cv.dim(ret)
-    assert(abs.w==30 and abs.h==20)
-end
-
-print "pico.xin.dim - %"
-do
-    local out   = {'!', x=20, y=20, w=60, h=60, anchor='NW'}
-    local child = {'%', w=0.5, h=0.5}
-    local ret = pico.xin.dim(out, child)
-    local abs = pico.cv.dim(ret)
-    assert(abs.w==30 and abs.h==30)
-end
-
-print "mode and anchor preserved"
+print "pico.xin.rect smoke"
 do
     local out   = {'!', x=20, y=20, w=60, h=60, anchor='NW'}
     local child = {'%', x=0.5, y=0.5, w=0.5, h=0.5, anchor='C'}
     local ret = pico.xin.rect(out, child)
     assert(ret[1] == '%')
     assert(ret.anchor.x == 0.5 and ret.anchor.y == 0.5)
+    local abs = {'!', anchor='NW'}
+    pico.cv.rect.to(nil, ret, abs)
+    assert(math.floor(abs.x+0.5)==35 and math.floor(abs.y+0.5)==35)
+    assert(math.floor(abs.w+0.5)==30 and math.floor(abs.h+0.5)==30)
 end
 
-print "equivalence: xin matches cv with out_abs as base"
+print "pico.xin.pos smoke"
 do
-    local out   = {'%', x=0.5, y=0.5, w=0.5, h=0.5, anchor='C'}
-    local child = {'!', x=5, y=5, w=10, h=10, anchor='NW'}
-    local out_abs = pico.cv.rect(out)
-    local old_way = pico.cv.rect(child, nil, out_abs)
-    local ret = pico.xin.rect(out, child)
-    local new_way = pico.cv.rect(ret)
-    assert(old_way.x == new_way.x and old_way.y == new_way.y
-       and old_way.w == new_way.w and old_way.h == new_way.h)
+    local out   = {'!', x=20, y=20, w=60, h=60, anchor='NW'}
+    local child = {'%', x=0.5, y=0.5, anchor='NW'}
+    local ret = pico.xin.pos(out, child)
+    assert(ret[1] == '%')
+    local abs = {'!', anchor='NW'}
+    pico.cv.pos.to(nil, ret, abs)
+    assert(math.floor(abs.x+0.5)==50 and math.floor(abs.y+0.5)==50)
+end
+
+print "pico.xin.dim smoke"
+do
+    local out   = {'!', x=20, y=20, w=60, h=60, anchor='NW'}
+    local child = {'%', w=0.5, h=0.5}
+    local ret = pico.xin.dim(out, child)
+    assert(ret[1] == '%')
+    local abs = {'!'}
+    pico.cv.dim.to(nil, ret, abs)
+    assert(math.floor(abs.w+0.5)==30 and math.floor(abs.h+0.5)==30)
 end
 
 print "\n=== ALL TESTS PASSED ==="
--- tst/in.c
+++ tst/in.c
@@ -2,98 +2,21 @@
 #include <stdio.h>
 #include <assert.h>
 
-#define EPSILON 1
-
-static int pos_eq (Pico_Abs_Pos a, Pico_Abs_Pos b) {
-    return (abs(a.x - b.x) <= EPSILON) && (abs(a.y - b.y) <= EPSILON);
-}
-
-static int rect_eq (Pico_Abs_Rect a, Pico_Abs_Rect b) {
-    return (abs(a.x - b.x) <= EPSILON) && (abs(a.y - b.y) <= EPSILON) &&
-           (abs(a.w - b.w) <= EPSILON) && (abs(a.h - b.h) <= EPSILON);
-}
-
-static int dim_eq (Pico_Abs_Dim a, Pico_Abs_Dim b) {
-    return (abs(a.w - b.w) <= EPSILON) && (abs(a.h - b.h) <= EPSILON);
-}
-
-// resolve any rel into cur's frame as integer pixels (NW origin)
-static SDL_Point _abs_pos (const Pico_Rel_Pos* p) {
-    Pico_Rel_Pos out = { '!', {0, 0}, PICO_ANCHOR_NW };
-    pico_cv_pos_to(NULL, p, &out);
-    return (SDL_Point){ (int)(out.x + 0.5f), (int)(out.y + 0.5f) };
-}
-
-static SDL_Rect _abs_rect (const Pico_Rel_Rect* r) {
-    Pico_Rel_Rect out = { '!', {0, 0, 0, 0}, PICO_ANCHOR_NW };
-    pico_cv_rect_to(NULL, r, &out);
-    return (SDL_Rect){
-        (int)(out.x + 0.5f), (int)(out.y + 0.5f),
-        (int)(out.w + 0.5f), (int)(out.h + 0.5f),
-    };
-}
-
-static Pico_Abs_Dim _abs_dim (const Pico_Rel_Dim* d) {
-    Pico_Rel_Dim out = { '!', {0, 0} };
-    pico_cv_dim_to(NULL, d, &out);
-    return (Pico_Abs_Dim){
-        (int)(out.w + 0.5f), (int)(out.h + 0.5f),
-    };
-}
-
 int main (void) {
     pico_init(1);
     Pico_Rel_Dim log = { '!', {100, 100} };
     pico_set_scene_dim(&log);
 
-    // RECT IN RECT - ! NW
-    {
-        puts("rect in rect - ! NW");
-        Pico_Rel_Rect out = { '!', {20, 20, 60, 60}, PICO_ANCHOR_NW };
-        Pico_Rel_Rect in  = { '!', {10, 10, 20, 20}, PICO_ANCHOR_NW };
-        Pico_Rel_Rect ret = pico_in_rect(&out, &in);
-        SDL_Rect abs = _abs_rect(&ret);
-        printf("  ret: mode='%c' (%.2f,%.2f,%.2f,%.2f) abs (%d,%d,%d,%d)\n",
-               ret.mode, ret.x, ret.y, ret.w, ret.h,
-               abs.x, abs.y, abs.w, abs.h);
-        assert(rect_eq(abs, (Pico_Abs_Rect){30, 30, 20, 20}));
-    }
-
-    // RECT IN RECT - % C inside ! NW
+    // RECT IN RECT - % C inside ! NW (composition)
     {
         puts("rect in rect - % C in ! NW");
         Pico_Rel_Rect out = { '!', {20, 20, 60, 60}, PICO_ANCHOR_NW };
         Pico_Rel_Rect in  = { '%', {0.5, 0.5, 0.5, 0.5}, PICO_ANCHOR_C };
         Pico_Rel_Rect ret = pico_in_rect(&out, &in);
-        SDL_Rect abs = _abs_rect(&ret);
-        printf("  ret: mode='%c' abs (%d,%d,%d,%d)\n",
-               ret.mode, abs.x, abs.y, abs.w, abs.h);
-        assert(rect_eq(abs, (Pico_Abs_Rect){35, 35, 30, 30}));
-    }
-
-    // RECT IN RECT - % NW inside % C (nested pct)
-    {
-        puts("rect in rect - % in %");
-        Pico_Rel_Rect out = { '%', {0.5, 0.5, 0.5, 0.5}, PICO_ANCHOR_C };
-        Pico_Rel_Rect in  = { '%', {0.0, 0.0, 1.0, 1.0}, PICO_ANCHOR_NW };
-        Pico_Rel_Rect ret = pico_in_rect(&out, &in);
-        SDL_Rect abs = _abs_rect(&ret);
-        SDL_Rect out_abs = _abs_rect(&out);
-        printf("  out_abs (%d,%d,%d,%d) ret_abs (%d,%d,%d,%d)\n",
-               out_abs.x, out_abs.y, out_abs.w, out_abs.h,
-               abs.x, abs.y, abs.w, abs.h);
-        assert(rect_eq(abs, out_abs));
-    }
-
-    // POS IN RECT - ! NW
-    {
-        puts("pos in rect - ! NW");
-        Pico_Rel_Rect out = { '!', {20, 20, 60, 60}, PICO_ANCHOR_NW };
-        Pico_Rel_Pos  in  = { '!', {10, 10}, PICO_ANCHOR_NW };
-        Pico_Rel_Pos  ret = pico_in_pos(&out, &in);
-        SDL_Point abs = _abs_pos(&ret);
-        printf("  ret: mode='%c' abs (%d,%d)\n", ret.mode, abs.x, abs.y);
-        assert(pos_eq(abs, (Pico_Abs_Pos){30, 30}));
+        Pico_Rel_Rect abs = { '!', {0, 0, 0, 0}, PICO_ANCHOR_NW };
+        pico_cv_rect_to(NULL, &ret, &abs);
+        assert((int)(abs.x+0.5f)==35 && (int)(abs.y+0.5f)==35);
+        assert((int)(abs.w+0.5f)==30 && (int)(abs.h+0.5f)==30);
     }
 
     // POS IN RECT - % NW
@@ -102,20 +25,9 @@ int main (void) {
         Pico_Rel_Rect out = { '!', {20, 20, 60, 60}, PICO_ANCHOR_NW };
         Pico_Rel_Pos  in  = { '%', {0.5, 0.5}, PICO_ANCHOR_NW };
         Pico_Rel_Pos  ret = pico_in_pos(&out, &in);
-        SDL_Point abs = _abs_pos(&ret);
-        printf("  ret: mode='%c' abs (%d,%d)\n", ret.mode, abs.x, abs.y);
-        assert(pos_eq(abs, (Pico_Abs_Pos){50, 50}));
-    }
-
-    // DIM IN RECT - !
-    {
-        puts("dim in rect - !");
-        Pico_Rel_Rect out = { '!', {20, 20, 60, 60}, PICO_ANCHOR_NW };
-        Pico_Rel_Dim  in  = { '!', {30, 20} };
-        Pico_Rel_Dim  ret = pico_in_dim(&out, &in);
-        Pico_Abs_Dim abs = _abs_dim(&ret);
-        printf("  ret: mode='%c' abs (%d,%d)\n", ret.mode, abs.w, abs.h);
-        assert(dim_eq(abs, (Pico_Abs_Dim){30, 20}));
+        Pico_Rel_Pos abs = { '!', {0, 0}, PICO_ANCHOR_NW };
+        pico_cv_pos_to(NULL, &ret, &abs);
+        assert((int)(abs.x+0.5f)==50 && (int)(abs.y+0.5f)==50);
     }
 
     // DIM IN RECT - %
@@ -124,9 +36,9 @@ int main (void) {
         Pico_Rel_Rect out = { '!', {20, 20, 60, 60}, PICO_ANCHOR_NW };
         Pico_Rel_Dim  in  = { '%', {0.5, 0.5} };
         Pico_Rel_Dim  ret = pico_in_dim(&out, &in);
-        Pico_Abs_Dim abs = _abs_dim(&ret);
-        printf("  ret: mode='%c' abs (%d,%d)\n", ret.mode, abs.w, abs.h);
-        assert(dim_eq(abs, (Pico_Abs_Dim){30, 30}));
+        Pico_Rel_Dim abs = { '!', {0, 0} };
+        pico_cv_dim_to(NULL, &ret, &abs);
+        assert((int)(abs.w+0.5f)==30 && (int)(abs.h+0.5f)==30);
     }
 
     // MODE / ANCHOR PRESERVED FROM in
